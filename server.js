336506a12f6af63400089714e1fd2d57331491c9
Chris Owen <cowen1992@hotmail.co.uk>
2020-06-07 13:04:21 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const welcomeMessage = {
+  id: 0,
+  from: "Bart",
+  text: "Welcome to CYF chat system!",
+};
+
+//This array is our "data store".
+//We will start with one message in the array.
+//Note: messages will be lost when Glitch restarts our server.
+const messages = [welcomeMessage];
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.listen(process.env.PORT);
