7f287b6116bf85fe9179640665cb6d2128ae83ce
ferhatu <ferhat_2001@yahoo.com>
2020-07-02 17:05:52 +0100

    level-3

--- server.js
+++ server.js
@@ -1,23 +1,60 @@
 const express = require("express");
-const cors = require("cors");
+const bodyParser = require("body-parser")
+const cors = require('cors')
+const _ = require("lodash");
+let messages = require("./messages.json")
 
 const app = express();
 
 app.use(cors());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
+app.use(bodyParser.json());
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
-
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/index.html');
 });
 
-app.listen(process.env.PORT);
+//create new message
+app.post("/messages", function (request, response) {
+
+  if (request.body.text && request.body.from) {
+    messages.push(request.body);
+    response.send({ success: true });
+  }
+  else { response.send(400, "Message could not send !"); }
+
+})
+//read all messages
+app.get("/messages", function (request, response) {
+  response.send(messages)
+})
+
+// search text
+app.get('/messages/search', (request, response) => {
+  const searchTerm = request.query.text
+  response.json(messages.filter(item => item.text.toLowerCase().includes(searchTerm.toLowerCase())))
+})
+
+//getting latest 10 messages
+app.get("/messages/latest", function (request, response) {
+  response.send(_.takeRight(messages, 10));
+})
+
+//search by Id
+app.get('/messages/:id', function (request, response) {
+  const personId = Number(request.params.id);
+  const selectById = messages.find(item => item.id === personId);
+  selectById ? response.send(selectById) : response.status(404).send("No message found");
+
+})
+//delete by Id
+app.delete("/messages/:id", function (request, response) {
+  const personId = Number(request.params.id);
+  messages = messages.filter(item => item.id !== personId);
+  response.send({ success: true })
+})
+
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
