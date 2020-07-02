30910d991b5d2200613ef46e535033d9a3f17f49
byohannes <42332389+byohannes@users.noreply.github.com>
2020-07-02 02:52:04 +0100

    Level 2 simple validation and updatting chats also done

--- server.js
+++ server.js
@@ -5,27 +5,52 @@ const bodyParser = require ('body-parser');
 const app = express ();
 
 app.use (bodyParser.json ());
+app.use (express.urlencoded ({extended: false}));
 app.use (cors ());
 
-const welcomeMessage = {
-  id: 0,
-  from: 'Bart',
-  text: 'Welcome to CYF chat system!',
-};
-
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [
+  {
+    id: 0,
+    from: 'Bart',
+    text: 'Welcome to CYF chat system!',
+  },
+  {
+    id: 1,
+    from: 'Bani',
+    text: 'Gd Afternoon',
+  },
+  {
+    id: 2,
+    from: 'Nader',
+    text: 'I am having an exercise',
+  },
+  {
+    id: 3,
+    from: 'Neil',
+    text: 'Hello CYF!',
+  },
+];
 
 app.get ('/', function (request, response) {
   response.sendFile (__dirname + '/index.html');
 });
 //  Create a new message
-app.post ('/messages', (req, res) => {
-  const message = req.body;
-  messages.push (message);
-  res.json (message).status (201);
+app.post ('/messages/create', (req, res) => {
+  const newId = messages.length + 1;
+  const newIdExist = messages.find (msg => msg.id === newId);
+  if (newIdExist) {
+    newId++;
+  } else {
+    if (req.body.text === '' || req.body.from === '') {
+      res.status (400).json ('Please Fill all the form fields, thanks!');
+    } else {
+      req.body.id = newId;
+      messages.push (req.body);
+      res
+        .status (201)
+        .json (` Successfully A new chat with Id number ${newId}  is created.`);
+    }
+  }
 });
 
 //  Read All messages
@@ -41,7 +66,7 @@ app.get ('/messages/:id', (req, res) => {
 });
 
 //  Delete a message by ID
-app.delete ('/messages/:id', (req, res) => {
+app.delete ('/messages/delete/:id', (req, res) => {
   const {id} = req.params;
   const indexToDelete = messages.findIndex (msg => msg.id === Number (id));
   if (indexToDelete) {
@@ -53,15 +78,17 @@ app.delete ('/messages/:id', (req, res) => {
 });
 
 //  update a message by ID
-app.put ('/messages/:id', (req, res) => {
+app.put ('/messages/update/:id', (req, res) => {
   const {id} = req.params;
 
   const updatedMessage = req.body;
 
-  let existingMessage = messages.find (msg => msg.id === Number (id));
+  const existingMessage = messages.find (msg => msg.id === Number (id));
   if (existingMessage) {
-    existingMessage = updatedMessage;
-    res.json (existingMessage);
+    const filteredMsg = messages.filter (msg => msg.id !== Number (id));
+    messages = filteredMsg;
+    messages.push (updatedMessage);
+    res.json (`Successfully, A  chat with id number ${id} is updated.`);
   } else {
     res.sendStatus (404);
   }
