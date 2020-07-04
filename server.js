24cf64f7a465688f3fc48cdb4ef7b74806f88fd9
Babak <babakjahangiri@gmail.com>
2020-07-04 21:43:38 +0100

    level1 done

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const body = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
+app.use(body.json());
+app.use(express.urlencoded({ extended: false }));
+
+let MessageId = 0;
 
 const welcomeMessage = {
   id: 0,
@@ -16,8 +21,39 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.get("/messages/:id", (req, res) => {
+  const msgId = req.params.id;
+  const msg = messages.filter((m) => Number(m.id) === Number(msgId));
+  res.send(msg);
+});
+
+app.post("/messages", (req, res) => {
+  messages.push({
+    id: (MessageId += 1),
+    from: req.body.from,
+    text: req.body.text,
+  });
+  res.send({ insertResult: true });
+});
+
+app.delete("/messages/:id", (req, res) => {
+  const msgId = Number(req.params.id);
+  messages = messages.filter((m) => Number(m.id) !== Number(msgId));
+  res.send({ deleteResult: true });
+});
+
+app.get("/messages", function (req, res) {
+  res.json(messages);
+});
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log("server has started ...")
+);
