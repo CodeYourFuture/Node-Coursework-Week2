055631a1e3d788bfc31983c10b393cf805bfb499
Halit <60817499+halitos@users.noreply.github.com>
2020-08-18 23:15:10 +0100

    first commit

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(cors());
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded());
+
 const welcomeMessage = {
   id: 0,
   from: "Bart",
@@ -20,4 +24,24 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+// Get all messages
+app.get("/messages", (req, res) => {
+  res.json(messages);
+});
+
+//Send a new message
+app.post("/messages", (req, res) => {
+  const newMessage = {
+    id: messages.length,
+    from: req.body.from,
+    text: req.body.text,
+  };
+  console.log(req.body);
+  messages.push(newMessage);
+  res.json(messages);
+});
+
+//Read one message by id
+
+const PORT = 8080;
+app.listen(process.env.PORT || PORT);
