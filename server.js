837b252cbb8a981f429c37f900db56c44b433953
F Nawal Abdul Careem <nawal@fs-macbook-air.home>
2020-08-18 23:09:21 +0100

    level 1 complete

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 
-app.use(cors());
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 const welcomeMessage = {
   id: 0,
@@ -20,4 +24,25 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.post("/messages", (req, res)=>{
+  
+  let newPost ={
+    id:messages.length +1 ,
+    from: req.body.from,
+    text: req.body.text
+  }
+  
+  messages.push(newPost)
+  res.json(messages)
+})
+
+
+//read  messages
+
+app.get("/messages", (req, res) => {
+ res.json(messages);
+});
+
+const listener = app.listen(process.env.PORT || 3005, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
