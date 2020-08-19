d1eb1e9f53040b0cffe430b5801ce7c1d637e4cd
F Nawal Abdul Careem <nawal@fs-macbook-air.home>
2020-08-19 07:56:10 +0100

    completed level 1

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
-
-
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 
 const welcomeMessage = {
@@ -23,7 +20,7 @@ const messages = [welcomeMessage];
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
-
+//create a new message
 app.post("/messages", (req, res)=>{
   
   let newPost ={
@@ -42,6 +39,30 @@ app.post("/messages", (req, res)=>{
 app.get("/messages", (req, res) => {
  res.json(messages);
 });
+
+///Read one message specified by an ID
+// app.get("/messages/:id", (req, res)=>{
+//   let id = Number(req.params.id);
+//   let result = messages.find(message => message.id === id);
+//   if(result){
+//     res.json(result)
+//   }else{
+//     res.status(400).json("what's hell you mean!");
+//   }
+// })
+
+// Delete a message, by ID
+
+app.delete("/messages/delete/:id", (req, res)=>{
+ let id = Number(req.params.id);
+ let index = messages.findIndex(message =>message.id === id);
+ if(index){
+  messages.splice(index, 1);
+  res.json(`message with id: ${id} has been deleted`);
+ }else{
+  res.status(404).json("Please enter valid id to delete");
+ }
+})
 
 const listener = app.listen(process.env.PORT || 3005, function() {
   console.log("Your app is listening on port " + listener.address().port);
