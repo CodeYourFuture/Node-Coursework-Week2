f6cc05dabb2e9a5485b6821f6ea656021f2923dc
Taslima <61126921+Taslima26@users.noreply.github.com>
2020-08-19 21:40:29 +0100

    routing fixed here

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require ('express');
-const cors = require ('cors');
-const Joi = require ('joi');
+const express = require('express');
+const cors = require('cors');
+const Joi = require('joi');
 
-const app = express ();
-app.use (express.json ());
+const app = express();
+app.use(express.json());
 
-app.use (cors ());
+app.use(cors());
 
 const welcomeMessage = {
   id: 0,
   from: 'Bart',
   text: 'Welcome to CYF chat system!',
-  timeStamp: new Date ().toLocaleString (),
+  timeStamp: new Date().toLocaleString(),
 };
 
 //This array is our "data store".
@@ -23,127 +23,128 @@ const messages = [
     id: 1,
     from: 'Taslima',
     text: 'Welcome to our code your future',
-    timestamp: new Date ().toLocaleString (),
+    timestamp: new Date().toLocaleString(),
   },
   {
     id: 2,
     from: 'Aysha',
     text: 'Welcome to my nursery!!!',
-    timestamp: new Date ().toLocaleString (),
+    timestamp: new Date().toLocaleString(),
   },
   {
     id: 3,
     from: 'Khadijah',
     text: 'Welcome to my school!!!',
-    timestamp: new Date ().toLocaleString (),
+    timestamp: new Date().toLocaleString(),
   },
 ];
 
-app.get ('/', function (request, response) {
-  response.sendFile (__dirname + '/index.html');
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/index.html');
 });
 
 //read all messages
-app.get ('/messages', function (request, response) {
-  response.send (messages);
+app.get('/messages', function (request, response) {
+  response.send(messages);
 });
 
 //create a new message.
-app.post ('/messages', (request, response) => {
+app.post('/messages', (request, response) => {
   //error handling using joi package manager
   const schema = {
-    from: Joi.string ().min (3).required (),
-    text: Joi.string ().min (3).required (),
+    from: Joi.string().min(3).required(),
+    text: Joi.string().min(3).required(),
   };
-  const result = Joi.validate (request.body, schema);
+  const result = Joi.validate(request.body, schema);
   if (result.error) {
-    response.status (400).send (result.error.details[0].message);
+    response.status(400).send(result.error.details[0].message);
     return;
   }
   const message = {
     id: messages.length + 1,
     from: request.body.from,
     text: request.body.text,
-    timestamp: new Date ().toLocaleString (),
+    timestamp: new Date().toLocaleString(),
   };
-  messages.push (message);
-  response.send (message);
+  messages.push(message);
+  response.send(message);
 });
 
-//read one message specified by id
-// app.get('/messages/:id', function (request, response) {
-//   const messageId = request.params.id;
-//   const result = messages.find((message) => message.id === parseInt(messageId));
-//   if (!result) {
-//     result.status(404).send('The message with given id is not found');
-//   }
-//   response.send(result);
-// });
 //delete a message by id
 //look up the course
 //not existing,return 404
 
-app.delete ('/messages/:id', (req, res) => {
+app.delete('/messages/:id', (req, res) => {
   const messageId = req.params.id;
   //validation
-  const message = messages.find (
-    message => message.id === parseInt (messageId)
+  const message = messages.find(
+    (message) => message.id === parseInt(messageId)
   );
   if (!message)
-    res.status (404).send ('The message with given Id does not exist');
+    res.status(404).send('The message with given Id does not exist');
   //delete
-  const index = messages.indexOf (message);
-  messages.splice (index, 1);
+  const index = messages.indexOf(message);
+  messages.splice(index, 1);
   //return response back to user
-  res.send (message);
+  res.send(message);
 });
 
 //search functionality
-app.get ('/messages/search', function (req, res) {
+app.get('/messages/search', function (req, res) {
   const {text} = req.query;
   let foundMessages = messages;
   if (text !== undefined) {
-    foundMessages = messages.filter (message =>
-      message.text.toLowerCase ().includes (text.toLocaleLowerCase ())
+    foundMessages = messages.filter((message) =>
+      message.text.toLowerCase().includes(text.toLocaleLowerCase())
     );
   }
 
-  res.send (foundMessages);
+  res.send(foundMessages);
 });
 
 //get the latest message functionality
-app.get ('/messages/latest', function (req, res) {
-  let result = messages.slice (-2);
-  res.send (result);
+app.get('/messages/latest', function (req, res) {
+  let result = messages.slice(-2);
+  res.send(result);
 });
 //update functionality
-app.put ('/messages/:id', (req, res) => {
+app.put('/messages/:id', (req, res) => {
   //look up the message if message does not exist return 404.
-  const message = messages.find (
-    message => message.id === parseInt (req.params.id)
+  const message = messages.find(
+    (message) => message.id === parseInt(req.params.id)
   );
   if (!message)
-    res.status (404).send ('The message with given id does not exist');
+    res.status(404).send('The message with given id does not exist');
 
   //validate course make sure if its in good shape
   const schema = {
-    from: Joi.string ().min (3).required (),
-    text: Joi.string ().min (3).required (),
+    from: Joi.string().min(3).required(),
+    text: Joi.string().min(3).required(),
   };
   //if invalid we return 400 bad request
-  const result = Joi.validate (req.body, schema);
+  const result = Joi.validate(req.body, schema);
   if (result.error) {
-    res.status (400).send (result.error.details[0].message);
+    res.status(400).send(result.error.details[0].message);
     return;
   }
   //update the course.
   message.text = req.body.text;
   message.from = req.body.from;
   //return the updated course to the client.
-  res.send (message);
+  res.send(message);
+});
+
+//read one message specified by id
+app.get('/messages/:id', function (request, response) {
+  const messageId = request.params.id;
+  const result = messages.find((message) => message.id === parseInt(messageId));
+  if (!result) {
+    result.status(404).send('The message with given id is not found');
+  }
+  response.send(result);
 });
 
 const port = process.env.PORT || 3000;
-app.listen (port, () => {
-  console.log (` up and running! on port${port}`);
+app.listen(port, () => {
+  console.log(` up and running! on port${port}`);
 });
