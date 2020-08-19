b350d788c527f402bbc2313ee9a3f03054811192
Osman Elsahib <elsahib10@gmail.com>
2020-08-19 21:55:30 +0100

    up to level 4

--- server.js
+++ server.js
@@ -20,4 +20,57 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
+app.get('/messages', function (request, response) {
+  response.json(messages)
+})
+
+app.post('/messages', function (request, response) {
+  let newMessage = {};
+
+  if (request.body.from.trim() && request.body.text.trim()) {
+    newMessage.id = messages.length
+    newMessage.from = request.body.from
+    newMessage.text = request.body.text
+    newMessage.timeSent = new Date()
+    messages.push(newMessage)
+    response.json(messages)
+  } else {
+    response.status(400).json("Please fill all fileds")
+  }
+
+})
+
+app.get('/messages/search', function (request, response) {
+  let searchWord = request.query.text
+  let result = messages.filter(item => item.text.toLowerCase().includes(searchWord.toLowerCase()))
+  if (result.length > 0) {
+    response.json(result)
+  } else {
+    response.status(404).json("Nothing was found")
+  }
+})
+
+app.get('/messages/latest', function (request, response) {
+  let latestMessages = messages.splice(-2)
+  response.json(latestMessages)
+})
+
+app.get('/messages/:id', function (request, response) {
+  let id = parseInt(request.params.id)
+  let result = messages.find(item => item.id === id)
+  if (result !== undefined) {
+    response.json(result)
+  } else {
+    response.json("not found")
+  }
+})
+
+app.delete('/messages/:id', function (request, response) {
+  let id = parseInt(request.params.id),
+    result = messages.find(item => item.id === id),
+    index = messages.indexOf(result);
+  messages.splice(index, 1)
+  response.json(messages)
+})
+
 app.listen(process.env.PORT);
