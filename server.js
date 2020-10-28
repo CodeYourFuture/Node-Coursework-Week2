cf98dcb7cbe53dc25a75f3c85c9a65dae1fa7649
edksam <paakofisam.sam@gmail.com>
2020-10-28 14:24:25 +0000

    Main chat routing done.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 
+const port = process.env.PORT || 3333;
+
+const bodyParser = require("body-parser");
+
 const app = express();
 
 app.use(cors());
@@ -19,5 +23,34 @@ const messages = [welcomeMessage];
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+app.get("/chat", function (request, response) {
+  response.json(messages);
+});
 
-app.listen(process.env.PORT);
+//Create/update = POST
+app.post("/chat", (request, response) => {
+  const msg = request.body;
+  messages.push(msg);
+  response.json("Message Added ");
+  console.log(msg);
+});
+app.get("/chat/:id", function (req, res) {
+  let { id } = req.params;
+
+  const msg = messages.find((msg) => msg.id == id);
+  res.json(id);
+  // req.params.albumId will match the value in the url after /albums/
+  console.log(req.params.albumId);
+});
+
+app.delete("/chat/:id", function (req, res) {
+  const { id } = req.params;
+  messages.forEach((msg) => {
+    if (msg.albumId == id) {
+      messages.splice(msg, 1);
+    }
+  });
+  res.json("Chat deleted sucessfully.");
+});
+
+app.listen(port);
