0226184592c3e2afebc7968b769ca98d284d4ab5
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-10-30 19:37:26 +0000

    Added cors for front end

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-//const cors = require("cors");
-//app.use(cors());
+const cors = require("cors");
+app.use(cors());
+
+
 
 let messages = require("./messages.json");
 
