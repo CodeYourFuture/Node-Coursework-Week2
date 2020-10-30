b31f0ed70ce772f9f1a412cc418e8f0eae1d44e0
edksam <paakofisam.sam@gmail.com>
2020-10-30 23:48:08 +0000

    delete amended

--- server.js
+++ server.js
@@ -1,56 +1,122 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 4444;
 
 const bodyParser = require("body-parser");
 
+let messages = require("./messages.json");
+
 const app = express();
+
+const save = () => {
+  fs.writeFile(
+    "./messages.json",
+    JSON.stringify(messages, null, 2),
+    (error) => {
+      if (error) {
+        throw error;
+      }
+    }
+  );
+};
+app.use(express.static(".index.html"));
 
 app.use(cors());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
+app.use(bodyParser.json());
+// const welcomeMessage = {
+//   id: 0,
+//   from: "Bart",
+//   text: "Welcome to CYF chat system!",
+// };
 
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+// const messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
-app.get("/chat", function (request, response) {
+
+app.get("/messages", function (request, response) {
   response.json(messages);
+  response.send(`All messages are ${messages.length}`);
+});
+
+app.get("/messages/latest", function (request, response) {
+  response.json(messages.slice(-10));
+  response.send("Latest Found");
 });
 
 //Create/update = POST
-app.post("/chat", (request, response) => {
-  const msg = request.body;
-  messages.push(msg);
-  response.json("Message Added ");
-  console.log(msg);
+app.post("/message", (request, response) => {
+  // const message = request.body;
+  // const message = {};
+  message.id = Id;
+  message.from = req.body.from;
+  message.text = req.body.text;
+  if (message.from != "" && message.text != "") {
+    messages.push(message);
+    console.log(message);
+    Id++;
+    save();
+    response.json({
+      status: "success",
+      message: request.body,
+    });
+  } else {
+    res.sendStatus(400);
+  }
 });
-app.get("/chat/:id", function (req, res) {
-  let { id } = req.params;
+app.get("/message/:id", function (request, response) {
+  let { id } = request.params;
 
-  const msg = messages.find((msg) => msg.id == id);
-  res.json(id);
+  const messages = messages.find((msg) => msg.id == id);
+  response.json(id);
   // req.params.albumId will match the value in the url after /albums/
-  console.log(req.params.albumId);
+  console.log(request.params.id);
 });
 
-app.delete("/chat/:id", function (req, res) {
+app.delete("/messages/:id", (request, response) => {
   const { id } = req.params;
   messages.forEach((msg) => {
-    if (msg.albumId == id) {
+    if (msg.id == id) {
       messages.splice(msg, 1);
     }
   });
-  res.json("Chat deleted sucessfully.");
+  // // const id = request.params.id;
+  // messages = messages.filter((def) => {
+  //   if (def.id !== request.params.id) {
+  //     return true;
+  //   }
+  //   return false;
+  // });
+  response.json({
+    status: "success",
+    removed: request.params.id,
+    newLength: messages.length,
+  });
+});
+
+//Search
+app.get("/messages/search", (request, response) => {
+  const term = request.query.term;
+  const search = messages.filter(
+    (item) =>
+      item.from.toLowerCase().includes(term.toLowerCase()) ||
+      item.text.toLowerCase().includes(term.toLowerCase())
+  );
+  if (term != null) {
+    response.send(search);
+  } else {
+    response.end();
+  }
+
+  // let term = req.query.search;
+  // res.send(`${searchQuery}`);
 });
 
 app.listen(port);
