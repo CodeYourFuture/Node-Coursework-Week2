a636b7c94ae307f2673cff776813d2d5c48fa8fc
DenizAri <58032174+DenizAri@users.noreply.github.com>
2020-10-30 12:10:18 +0000

    Chatserver Level1-2-3 done

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 app.use(cors());
 
 const welcomeMessage = {
@@ -20,4 +21,48 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", function (request, response) {
+  response.json(messages);
+});
+// Read only the most recent 10 messages
+app.get('/messages/latest', function (req, res){
+  const recentMessages = messages.filter(i => i.id <= 10);
+  res.send(recentMessages);
+});
+
+// app.post("/messages", function (request, response) {
+//   let newMessage = request.body;
+//   console.log(request.body);
+//   newMessage.id = messages.length;
+//   messages.push(newMessage);
+//   response.send("Message submitted");
+// });
+app.post("/messages", function (request, response) {
+  const newMessage = request.body;
+  if (
+    typeof newMessage.text == "string" &&
+    newMessage.text.length > 0 &&
+    typeof newMessage.from == "string" &&
+    newMessage.from.length > 0
+  ) {
+    const id = messages.length;
+    newMessage.id = id;
+    messages.push(newMessage);
+    response.json({ success: true });
+  } else {
+    response.status(400).send("Please add a proper message");
+  }
+});
+
+app.get("/messages/:id", function (request, response) {
+  let id=request.params.id;
+  let foundMessage=messages.find(item => item.id ==id)
+  response.json(foundMessage);
+  
+  app.delete("/messages/:id", function (request, response) {
+const {id} = request.params;
+messages= messages.filter((message) => message.id !=id)
+response.json(messages);
+  })
+  app.listen(port);
+  console.log("Port listening")
