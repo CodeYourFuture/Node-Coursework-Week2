11c722a55c46e781e5cc492b5ff4203c810dc485
Amanuel <weditedi25@gmail.com>
2021-04-26 18:02:33 +0100

    initial commit and package installed

--- server.js
+++ server.js
@@ -20,4 +20,5 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on ${port}`));
