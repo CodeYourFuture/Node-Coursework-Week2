890d48dff1b2e960a152c5260f8811ab863eafad
jon <jonprosser84@gmail.com>
2021-04-27 19:32:54 +0100

    nearlt completed level 1 and 2

--- server.js
+++ server.js
@@ -20,4 +20,32 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+
+// send a message confirming the server works
+app.get("/test", (req, res) => {
+  res.status(200).send('Hello from the server');
+});
+
+
+//sends the messages back as stored in messages object.
+app.get("/messages", (req, res) => {
+  res.status(200).send(messages);
+});
+
+//read one message as specified by id
+app.get("/messages/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const message = messages.find((msg) => msg.id === id);
+
+  message ? res.status(200).send(message) : res.status(404).send('Message not found');
+});
+
+//create a message
+app.use(express.json());
+app.post("/message", (req, res) => {
+  const newMessage = req.body;
+
+
+})
+
+app.listen(process.env.PORT || 3000);
