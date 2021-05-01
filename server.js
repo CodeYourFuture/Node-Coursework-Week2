b941e14a74c73fa45adceb99e98ab7d6ddba72cf
JK1320 <kaurjus1@gmail.com>
2021-05-01 14:58:04 +0100

    Complete - Level 1

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const { response } = require("express");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 const welcomeMessage = {
   id: 0,
@@ -20,4 +23,31 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", function(req, res) { 
+  res.json(messages)
+})
+
+app.get("/messages/:id", function(req, res) { 
+  const id = parseInt(req.params.id);
+  const filterId = messages.find(item => item.id === id);
+  if(filterId){
+    res.json(filterId)
+  } else {
+    res.sendStatus(404);
+  }  
+})
+
+app.post("/messages", (req, res) => {
+  const newMessage = req.body;
+  newMessage.id = messages.length;
+  messages.push(newMessage);
+  res.send("Created");
+})
+
+app.delete("/messages/:id", function(req, res){
+   messagesData = messages.filter(message => message.id !== req.params.id);
+    res.status(204); 
+    res.end();
+})
+
+app.listen(process.env.PORT || 3000);
