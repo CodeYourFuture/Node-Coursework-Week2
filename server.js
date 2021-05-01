0a1b18e4c9bdf782702d3170b10b95b848776742
SenaitEkubai <Senaitekubai@gmail.com>
2021-05-01 13:08:24 +0100

    changed port number to 5000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { json } = require("body-parser");
 const { request, response } = require("express");
 const express = require("express");
 //const cors = require("cors");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
