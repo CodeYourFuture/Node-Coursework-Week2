0ca1659a4317e461073ed98128d1860796ab0442
yunusfirat <yunusfirat44@gmail.com>
2021-05-26 00:52:10 +0100

    removed some unneccessary data

--- Messages.js
+++ Messages.js
@@ -3,63 +3,7 @@ const messages = [
         id: 0,
         from: "Bart",
         text: "Welcome to CYF chat system!",
-    },
-    {
-        id: 1,
-        from: "Bart",
-        text: "Welcome to CYF chat system!",
-    },
-    {
-        id: 2,
-        from: "Bart",
-        text: "Welcome to CYF chat system!",
-    },
-    {
-        id: 3,
-        from: "Bart",
-        text: "Welcome to CYF chat system!",
-    },
-    {
-        id: 4,
-        from: "Bart",
-        text: "Welcome to CYF chat system!",
-    },
-    {
-        id: 5,
-        from: "Bart",
-        text: "Welcome to CYF chat system!",
-    },
-    {
-        id: 6,
-        from: "Bart",
-        text: "Welcome to CYF chat system!",
-    },
-    {
-        id: 7,
-        from: "Bart",
-        text: "Welcome to CYF chat system!",
-    },
-    {
-        id: 8,
-        from: "Bart",
-        text: "Welcome to CYF chat system!",
-    },
-    {
-        id: 9,
-        from: "Bart",
-        text: "Welcome to CYF chat system!",
-    },
-    {
-        id: 10,
-        from: "Bart",
-        text: "Welcome to CYF chat system!",
-    },
-    {
-        id: 11,
-        from: "Bart",
-        text: "Welcome to CYF chat system!",
-    },
-
+    }
 ];
 
 
