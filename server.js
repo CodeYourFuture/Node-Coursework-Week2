609d1ccb5d076a1f412de2b2e3f193159a78e4ae
Ionut Tripa <ionut.tripa.23@gmail.com>
2021-05-25 19:01:31 +0100

    testing

--- server.js
+++ server.js
@@ -4,20 +4,41 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+app.get("/", function (request, response) {
+	response.sendFile(__dirname + "./index.html");
+});
 
 const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
+	id: 0,
+	from: "Bart",
+	text: "Welcome to CYF chat system!",
 };
 
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
+
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "./index.html");
-});
+app.get('/search', (req, res) => {
+	console.log(req.query)
+})
 
-app.listen(process.env.PORT);
+app.post('/messages', (req, res) => {
+	console.log(req.body)
+	const newMessage = {
+		id: Number(messages.length),
+		from: req.body.from,
+		text: req.body.text
+	}
+	messages.push(newMessage);
+	res.status(201).send(messages);
+})
+
+app.get('/messages', (req, res) => {
+	res.json(messages)
+})
+
+app.listen(3000, () => console.log('Listening on port 3000'));
