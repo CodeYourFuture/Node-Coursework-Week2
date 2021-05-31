ff1be368ec1ab2bf81750469163ddadaefa654bd
askinekinci <askinekincihf@gmail.com>
2021-05-31 12:34:26 +0100

    completed Level 1 and Level 2

---
+++ Messages.js
@@ -0,0 +1,28 @@
+//This array is our "data store".
+//We will start with one message in the array.
+//Note: messages will be lost when Glitch restarts our server.
+
+let messages = [
+  {
+    id: 0,
+    from: "Bart",
+    text: "Welcome to CYF chat system!"
+  },
+  {
+    id: 1,
+    from: "Neil",
+    text: "Welcome to CYF chat system!"
+  },
+  {
+    id: 2,
+    from: "Lucy",
+    text: "Welcome to CYF chat system!"
+  },
+  {
+    id: 3,
+    from: "Thomas",
+    text: "Welcome to CYF chat system!"
+  },
+]
+
+module.exports = messages;
--- server.js
+++ server.js
@@ -1,23 +1,84 @@
 const express = require("express");
-const cors = require("cors");
-
+const cors = require('cors')
 const app = express();
+const messages = require("./Messages")
+app.use(cors())
 
-app.use(cors());
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
-
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+//Upload index.html file
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/index.html');
 });
 
-app.listen(process.env.PORT);
+
+// Read all messages
+app.get("/messages", (request, response) => {
+  response.json(messages)
+});
+
+
+// Search by a term
+app.get("/messages/search", (request, response) => {
+  const term = request.query.term;
+
+  const filteredMessage = messages.filter(message => message.text.toLowerCase().includes(term.toLowerCase()) || message.from.toLowerCase().includes(term.toLowerCase()));
+
+  response.send(filteredMessage)
+})
+
+
+// Create a new message
+app.post("/messages", (request, response) => {
+  const newMessage = {
+    id: request.body.id,
+    from: request.body.from,
+    text: request.body.text
+  }
+
+  const uniqueMessageIdCheck = messages.some((message) => message.id === request.body.id)
+
+  if (!newMessage.from || !newMessage.text) {
+    return response.status(400).json({ msg: "Please include an author and message text" })
+  }
+
+  if (uniqueMessageIdCheck) {
+    return response.status(400).json({ msg: "Message Id is already created, please select another message id" })
+  }
+
+  messages.push(newMessage);
+  response.json(messages);
+});
+
+
+// Delete messages
+app.delete("/messages/:id", (request, response) => {
+  let isMessageIdFound = messages.some(message => message.id === parseInt(request.params.id))
+
+  if (isMessageIdFound) {
+    response.json({ msg: "Message deleted", messages: messages.filter((message) => message.id !== parseInt(request.params.id)) })
+  } else {
+    response.status(400).json({ msg: `No member with the id of ${request.params.id}` })
+  }
+})
+
+
+// Read one message specified by an ID
+app.get("/messages/:id", (request, response) => {
+  const isMessageIdFound = messages.some(message => message.id === parseInt(request.params.id))
+
+  if (isMessageIdFound) {
+    response.json(messages.filter((message) => message.id === parseInt(request.params.id)))
+  } else {
+    response.status(400).json({ msg: `No member with the id of ${request.params.id}` })
+  }
+});
+
+
+// app.listen(process.env.PORT);
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
