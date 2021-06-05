bc7d5a1715ce8905fbc8e4bd18b08d07bebef876
NeginDn <negin.derakhshaan@gmail.com>
2021-06-05 17:21:43 +0100

    Level 5 + Adding Schema

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const Joi = require("joi");
 const cors = require("cors");
 const { response, request } = require("express");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -34,16 +35,16 @@ app.get("/messages/search", (request, response) => {
   response.send(filterMessages);
 });
 
-app.get("/messages/:id", (request, response) => {
+app.get("/messages/:id([0-9]+)", (request, response) => {
   const { id } = request.params;
-  // const messageIndex = messages.findIndex((element) => {
-  //   return element.id === parseInt(id);
-  // });
-  // response.json(messages[messageIndex]);
   const message = messages.find((element) => {
     return element.id === parseInt(id);
   });
   response.send(message);
+});
+
+app.get("/messages/latest", (request, response) => {
+  response.send(messages.slice(-10));
 });
 
 app.post("/messages", (request, response) => {
@@ -53,28 +54,80 @@ app.post("/messages", (request, response) => {
   const minutes = new Date().getMinutes();
   timeSent = `${hour}:${minutes}, ${date}`;
   message.timeSent = timeSent;
-  message.id = messages.length;
-  if (message.text && message.from) {
+  message.id = messages.length + 1;
+
+  const { error } = validateMessages(request.body);
+  if (error) {
+    response.status(400).send(error.details[0].message);
+    return;
+  } else {
     messages.push(message);
     response.send(messages);
-  } else {
-    response.sendStatus(400);
-    // response.send("Fill all the required fields!");
   }
 
-  response.json(messages);
+  // response.json(messages);
 });
 
 app.delete("/messages/:id", (request, response) => {
   const { id } = request.params;
+
+  const message = messages.find((element) => element.id === parseInt(id));
+  if (!message)
+    return response.status(404).send("Message with given id was not found");
+
   messages.map((element) => {
     element.id === parseInt(id) ? messages.splice(id, 1) : null;
   });
   response.send(`You deleted the message with id, ${id}`);
 });
 
-app.get("/messages/latest", (request, response) => {
-  response.send(messages.slice(-10));
+app.put("/messages/:id", (request, response) => {
+  const { id } = request.params;
+  const message = messages.find((element) => element.id === parseInt(id));
+  if (!message)
+    response.status(404).send("message with given id was not found ");
+
+  const schema = {
+    from: Joi.string().min(3).required(),
+
+    text: Joi.string().min(3).required(),
+  };
+  const result = Joi.validate(request.body, schema);
+  if (result.error) {
+    response.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  message.text = request.body.text;
+  message.from = request.body.from;
+  response.send(message);
 });
 
-app.listen(port);
+const validateMessages = (message) => {
+  const schema = {
+    from: Joi.string().min(3).required(),
+    id: Joi.required(),
+    text: Joi.string().min(3).required(),
+    timeSent: Joi.required(),
+  };
+
+  return Joi.validate(message, schema);
+};
+
+// app.put("/messages/:id", (request, response) => {
+//   const { id } = request.params;
+
+//   const message = messages.find((element) => element.id === parseInt(id));
+//   if (!message)
+//     response.status(404).send("message with given id was not found ");
+
+//   if (request.body.text && request.body.from) {
+//     message.text = request.body.text;
+//     message.from = request.body.from;
+//     response.send(message);
+//   } else {
+//     response.sendStatus(400).send("Fill all the required fields!");
+//   }
+// });
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
