d9c84de85e1a4188cc45c9beb6ac9a6627f6a406
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-03 19:46:20 +0200

    install dependencies

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 const welcomeMessage = {
@@ -20,4 +20,4 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.listen(PORT);
