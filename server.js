2c0a452a988a3264fd0d0d50867d5450c163fe38
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-03 20:04:03 +0200

    get all messages

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 
 const welcomeMessage = {
@@ -15,6 +16,13 @@ const welcomeMessage = {
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
+
+// Read all messages
+app.get('/messages', (req, res) => {
+  res.json(messages);
+});
+
+
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
