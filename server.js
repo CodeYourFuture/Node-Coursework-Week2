245c6805bb4a1f3b3584e7891adeacc72c5744a4
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-04 15:37:43 +0200

    create a new message

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const _ = require("lodash");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 const welcomeMessage = {
@@ -17,12 +20,12 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-// Read all messages
+// Read All Messages
 app.get('/messages', (req, res) => {
   res.json(messages);
 });
 
-// Get message by ID
+// Get Message by ID
 app.get('/messages/:id', (req, res) => {
   const { id } = req.params;
   const matchedId = messages.some(message => message.id === parseInt(id)  );
@@ -35,6 +38,19 @@ app.get('/messages/:id', (req, res) => {
       msg: `No message with the id of ${id}` 
     })
   }
+});
+
+// Create a New Message
+app.post('/messages', (req, res) => {
+  const newMessage = {
+    id: _.uniqueId(),
+    from: req.body.from,
+    text: req.body.text
+  }
+
+  messages.push(newMessage);
+
+  res.send(messages);
 });
 
 app.get("/", function (request, response) {
