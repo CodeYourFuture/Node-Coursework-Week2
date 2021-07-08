11a000c9d3c886d28236fad75427220de5adf7b2
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-07-08 15:01:18 +0200

    Add route to read one message

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(cors());
 
 const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
+	id: 0,
+	from: "Bart",
+	text: "Welcome to CYF chat system!",
 };
 
 //This array is our "data store".
@@ -17,18 +17,29 @@ const welcomeMessage = {
 const messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+	response.sendFile(__dirname + "/index.html");
 });
 
 //Create new message
-app.post("/", (req,res)=>{
-  res.send("Message received");
-})
+app.post("/", (req, res) => {
+	res.send("Message received");
+});
 
 //Read all messages
-app.get("/messages", (req,res)=>{
-  res.json(messages);
-})
+app.get("/messages", (req, res) => {
+	res.json(messages);
+});
+
+//Read one message
+app.get("/messages/:id", (req, res) => {
+	messageId = req.params.id;
+	if (messageId) {
+		const filteredMessages = messages.filter((message) => message.id === id);
+		res.json(filteredMessages);
+	}else{
+    res.status(400).send("Not a valid id");
+  }
+});
 
 const listener = app.listen(process.env.PORT);
 console.log("Listening on", listener.address().port);
