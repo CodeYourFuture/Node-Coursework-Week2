64cbcd77e8d34fbd93e7351a71fd87dea469a859
Jontas77 <68595133+Jontas77@users.noreply.github.com>
2021-07-09 20:18:40 +0200

    Changed imports and port

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
