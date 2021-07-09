150bfd78c96fbc11c0848322ee65f9cac8db60e5
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-07-10 01:00:59 +0200

    updated server.js
    added utils

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
-// const formatMessage = require('./utils/messages');
-// const {
-//   userJoin,
-//   getCurrentUser,
-//   userLeave,
-//   getRoomUsers
-// } = require('./utils/users');
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
