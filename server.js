4ec679d892a457965157c11c78089199bd077bad
omerffaruk <farukyamak11044@gmail.com>
2021-11-08 17:14:58 +0000

    feat: send /messages, /messages/:id end points responses

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 const welcomeMessage = {
@@ -16,8 +19,22 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+// Read all messages
+app.get("/messages", function (req, res) {
+  res.send(messages);
+})
+
+// Read one message specified by an ID
+app.get("/messages/:id", function (req, res) {
+  const id = req.params.id;
+  const message = messages.filter((m) => m.id == id);
+  res.send(message);
+})
+
+app.listen(port, () => {
+  console.log(`The server is listening on PORT ${port}`);
+});
