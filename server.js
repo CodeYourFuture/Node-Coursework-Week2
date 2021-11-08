1270b4406cea9073b4af9fa52629e183aade9dd6
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-11-08 21:46:31 +0000

    Level 1 complete

--- server.js
+++ server.js
@@ -1,23 +1,60 @@
 const express = require("express");
 const cors = require("cors");
 
+// Import library to generate random id's.
+const { v4: uuidv4 } = require("uuid");
+
 const app = express();
 
 app.use(cors());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
+// Required when POST comes from a form
+app.use(express.urlencoded({ extended: false }));
+
+const welcomeMessage = [
+  {
+    id: 0,
+    from: "Bart",
+    text: "Welcome to CYF chat system!",
+  },
+  {
+    id: 1,
+    from: "Tom",
+    text: "Welcome to CYF chat system!",
+  },
+];
 
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+const messages = welcomeMessage;
 
+// Display root directory, input controls
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+// Get all messages
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+// Get message by id
+app.get("/messages/:id", (req, res) => {
+  res.send(messages[req.params.id]);
+});
+
+// Add new message
+app.post("/messages", (req, res) => {
+  const newMessage = {
+    id: uuidv4(),
+    from: req.body.from,
+    text: req.body.text,
+  };
+
+  messages.push(newMessage);
+
+  res.send("Message Added");
+});
+
+app.listen(process.env.PORT || 3001);
