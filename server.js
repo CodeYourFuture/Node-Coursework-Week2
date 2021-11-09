3ae3ac0fbcf4bc0d976ebc0072f7d89bdc34bfd4
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-11-09 12:25:46 +0000

    feat: get, post, delete added

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const welcomeMessage = {
   id: 0,
@@ -20,4 +24,31 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", (request, response) => {
+  response.send(messages);
+});
+
+app.get("/messages/:id", (request, response) => {
+  const id = +request.params.id;
+  const selectedId = messages.filter(message => message.id === id);
+  response.send(selectedId);
+})
+app.post("/messages", (request, response) => {
+  const newMessages = request.body;
+  const newObject= {
+    id: messages[messages.length-1].id + 1,
+    from: newMessages.from,
+    text: newMessages.text,
+}; 
+  messages.push(newObject);
+  response.send(newObject);
+});
+app.delete("/messages/:id", (request, response) => {
+  const id = +request.params.id;
+  const index = messages.findIndex(message => message.id === id);
+  messages.splice(index, 1);
+  response.send(`Message ${id} deleted`);
+});
+app.listen(PORT, function () {
+  console.log(`Server is listening on port ${PORT}. Ready to accept requests!`);
+});
