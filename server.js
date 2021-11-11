366df05e4ee725e9b33170f7beef77067baa9d0f
omerffaruk <farukyamak11044@gmail.com>
2021-11-11 07:23:32 +0000

    optional tasks completed

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const app = express();  
+const cors = require('cors');
 const port = process.env.PORT || 3000;
-
-const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+//This array is our "data store".
+//We will start with one message in the array.
+//Note: messages will be lost when Glitch restarts our server.
 
 const welcomeMessage = {
   id: 0,
@@ -14,10 +17,10 @@ const welcomeMessage = {
   text: "Welcome to CYF chat system!",
 };
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
+
+// app.use('/messages', require('./routes/messages/messages'));
+
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -27,17 +30,23 @@ app.get("/", function (req, res) {
 app.post("/messages", function (req, res) {
   const lastIndex = messages.length - 1;
   const lastId = messages[lastIndex].id;
+  const today = new Date();
+  const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+  const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+  const dateTime = `${date} ${time}`;
 
   const newMessage = {
     "id" : lastId + 1,
     "from": req.body.from,
-    "text": req.body.text
-  }
+    "text": req.body.text,
+    "timeSent": dateTime
+  };
+  
   if(!newMessage.from || !newMessage.text) {
     res.sendStatus(400);
   } else {
     messages.push(newMessage);
-    res.status(200);
+    res.status(200).send(messages);
   }
 })
 
@@ -46,12 +55,39 @@ app.get("/messages", function (req, res) {
   res.send(messages);
 })
 
+// Read messages whose text contains a given substring: `/messages/search?text=express`
+app.get('/messages/search', function (req, res) {
+  const searchText = req.query.text;
+  const foundMessages = messages.filter((msg) => msg.text.includes(searchText));
+  res.send(foundMessages);
+})
+
+// Read only the most recent 10 messages: `/messages/latest`
+app.get('/messages/latest', function (req, res) {
+  const latestTenMessages = messages.slice(-10)
+  res.send(latestTenMessages);
+})
+
 // Read one message specified by an ID
 app.get("/messages/:id", function (req, res) {
   const id = req.params.id;
   const message = messages.filter((m) => m.id == id);
   res.send(message);
 })
+
+// Update text or from property
+app.put('/messages/update/:id', function (req, res) {
+    const message = messages.find(({id}) => id === parseInt(req.params.id));
+    if(message === undefined) {
+       return res.status(400).send({ "msg": `No message with the id of ${req.params.id}`});
+    }
+    const updMessage = req.body;
+    message.from = updMessage.from;
+    message.text = updMessage.text;
+    res.send(message);
+})
+
+
 
 app.listen(port, () => {
   console.log(`The server is listening on PORT ${port}`);
