789e14c6f27c9b0239cdd86a5751370a59b39b3c
Peter Armstrong <peterarmstrong999@gmail.com>
2021-11-11 22:07:32 +0000

    Homework finished

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 const welcomeMessage = {
   id: 0,
@@ -15,9 +17,30 @@ const welcomeMessage = {
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
+let messageCounter = 1
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.post("/messages", function (request, response) {
+  console.log(request.body);
+  let newMessage = request.body
+  newMessage.id = messageCounter++
+  messages.push(newMessage)
+  console.log(JSON.stringify(messages));
+  response.sendStatus(201)
+});
+
+app.get("/messages", function (request, response) {
+  response.send(messages)
+});
+
+app.get("/messages/:id", function (request, response) {
+  const id = parseInt(request.params.id)
+  const message = messages.find(m => m.id === id)
+  response.send(message)
+
+})
+
+app.listen(process.env.PORT || 4000);
