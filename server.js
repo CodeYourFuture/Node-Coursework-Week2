16b5dc8b2e0cc2e8587ee563ff8669c0b9458c45
blasisi <Bimbolamarialasisi@gmail.com>
2021-11-22 19:16:19 +0000

    working on level 1

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const { response } = require("express");
 
 const app = express();
-
+const PORT = process.env.PORT || 3000;
 app.use(cors());
+app.listen(PORT);
+console.log(PORT);
 
 const welcomeMessage = {
-  id: 0,
+  id: 1,
   from: "Bart",
   text: "Welcome to CYF chat system!",
 };
@@ -14,10 +17,33 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
+
 const messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+//Create a new message
+
+// app.post("/messages",  (req, res) =>  {
+//   response.json(messages);
+// });
+
+// Read one message specified by an ID
+app.get("/messages/:id", (request, response) => {
+  //root
+  let { id } = request.params;
+  id = parseInt(id);
+  const findInboxMessage = messages.find((message) => message.id === id);
+  response.json(findInboxMessage);
+});
+
+//Delete a message, by ID
+// app.delete("/messages/:id", (request, response)=> {
+//   const
+// const deleteMessageId = messages.forEach((messages) =>{
+//   if(messages)
+// })
+
+// app.listen(process.env.PORT);
