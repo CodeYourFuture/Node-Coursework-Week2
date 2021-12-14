3b84c9b918f4d21dd202b771679e3df5700e18f8
maham511 <maha.malik511@gmail.com>
2021-12-14 21:28:46 +0000

    Level 3 done

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-
-//for use with forms
+//urlencoded for use with forms
 app.use(express.urlencoded({ extended: false }));
-
 
 const welcomeMessage = {
   id: 0,
@@ -24,23 +22,43 @@ app.get("/", function (request, response) {
 });
 
 //request to read all messages
-app.get("/messages", function (request, response) {
+app.get("/messages", (request, response) => {
   response.json(messages);
+});
+
+//read messages with substring
+// (search query get requests go above get requests with params)
+app.get("/messages/search", getMsg);
+
+function getMsg(req, res) {
+  let text = req.query.text; //query variable for user
+  text = text.toLowerCase(); //for case-insensitive search
+
+  //filters quotes arr, returns objs including search term in quote or author values, case insensitive
+  const filteredMsg = messages.filter((message) => {
+    return message.text.toLowerCase().includes(text);
+  });
+
+  res.json(filteredMsg);
+}
+
+app.get("/messages/latest", (request, response) => {
+  const maxMessages = messages.slice(0, 10);
+  response.json(maxMessages);
 });
 
 app.get("/messages/:msgId", (request, response) => {
   const msgId = request.params.msgId;
+
   if (msgId) {
     const filteredMsg = messages.filter((message) => message.id == msgId);
     return response.json(filteredMsg);
   }
 });
-// --------------------------------
 
 app.post("/messages", (request, response) => {
   const lastIndex = messages.length - 1;
   const lastId = messages[lastIndex].id;
-
   const newMessage = {
     id: lastId + 1,
     from: request.body.from,
