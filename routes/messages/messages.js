6a1ab181278b9906a9611cffca311185e1c4886d
omerffaruk <farukyamak11044@gmail.com>
2022-01-01 15:05:09 +0000

    feat: createDateAndTime module created

--- routes/messages/messages.js
+++ routes/messages/messages.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router =  express.Router();
+const createDateAndTime = require('../../utils/createDateAndTime');
 
 const welcomeMessage = {
   id: 0,
@@ -79,13 +80,5 @@ router.put('/update/:id', function (req, res) {
         });
     }
 })
-
-function createDateAndTime () {
-    const today = new Date();
-    const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
-    const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
-    const dateTime = `${date} ${time}`;
-    return dateTime;
-}
 
 module.exports = router;
