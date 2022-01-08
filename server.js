906f438808bca13a03e27d710f1c9840bdfe6eee
gynaa78 <gynaa78@yahoo.com>
2022-01-08 12:32:08 +0000

    Chat app in progress

--- server.js
+++ server.js
@@ -1,23 +1,59 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+
+const port = process.env.PORT || 5000;
+
+const bodyParser = require("body-parser");
+
+let messages = require("./messages.json");
 
 const app = express();
 
-app.use(cors());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
+const save = () => {
+  fs.writeFile(
+    "./messages.json",
+    JSON.stringify(messages, null, 2),
+  (err) => {
+    if (err) {
+      throw err;
+    }
+  });
 };
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", function (request, response) {
+  response.json(messages);
+});
+
+let Id = 1;
+app.post("/message", (request, response) => {
+  const message = {};
+  message.id = Id;
+  message.from = request.body.from;
+  message.text = request.body.text;
+
+  if (message.from != " " && message.text != "") {
+    messages.push(message);
+    Id = Id + 1;
+    save();
+    response.json(
+      {
+        status: "success",
+        message: "request.body",
+      }
+    );
+  } else {
+    response.json(400)
+}
+});
+
+app.listen(port);
