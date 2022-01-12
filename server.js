69bdc348af925dfe226824ae3a67475e8e7a6087
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2022-01-12 01:18:20 +0000

    Level 1 Complete

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+// Install, import and execute express in app.
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
-
+const PORT = process.env.PORT || 5003;
+const cors = require("cors");
 app.use(cors());
+
+// Set server to listen
+server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// GET index page at root
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
 
 const welcomeMessage = {
   id: 0,
@@ -11,13 +23,54 @@ const welcomeMessage = {
   text: "Welcome to CYF chat system!",
 };
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+// GET all messages
+app.get("/messages", function (req, res) {
+  res.send(messages);
 });
 
-app.listen(process.env.PORT);
+// GET by ID
+app.get("/messages/:id", (req, res) => {
+  const filteredMsg = messages.filter(
+    (message) => message.id == parseInt(req.params.id)
+  );
+
+  if (filteredMsg.length > 0) {
+    res.status(200).json(filteredMsg);
+  } else {
+    res
+      .status(400)
+      .send({ msg: `No message matching: ${req.params.id} has been found` });
+  }
+});
+
+// POST new message
+app.post("/messages", (req, res) => {
+  const newMessage = {
+    id: messages.length,
+    from: req.body.from,
+    text: req.body.text,
+  };
+
+  if (!newMessage.from || !newMessage.text) {
+    res.status(400).json({ msg: "Name and content required" });
+  }
+
+  messages.push(newMessage);
+  res.status(200).json(messages);
+});
+
+// DELETE a message
+app.delete("/messages/:id", (req, res) => {
+  const index = messages.findIndex(
+    (message) => message.id === parseInt(req.params.id)
+  );
+
+  if (index < 0) {
+    res.status(404).json({ msg: `No message found at index ${req.params.id}` });
+  }
+
+  messages.splice(parseInt(req.params.id), 1);
+  res.status(200).json(messages);
+});
