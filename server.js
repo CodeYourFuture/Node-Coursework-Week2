833b15df6b6f421c47e45692edaef26433b81c27
MireilleNyoubissi <nmireillemat@yahoo.fr>
2022-01-25 00:29:27 +0000

    Updated server

--- server.js
+++ server.js
@@ -16,6 +16,30 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
+//create a new message
+app.post('/', function (request, response) {
+  const newWelcomeMessage = {
+    id: uuid.v4(),
+    from: require.body.name,
+    text: require.body.message,
+  };
+  messages.push(newWelcomeMessage);
+  response.json(messages);
+});
+
+//Read all messages
+
+app.get('/', function(request, response){
+  response.json(messages.filter(message => message.text))
+})
+
+//Read single message specify by an id
+
+app.get('/:id', function(request, response){
+  
+})
+
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
