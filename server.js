59e70eca522dc435d7258e098b6f725940b7f3ba
craig-dsilva <90406619+craig-dsilva@users.noreply.github.com>
2022-05-08 12:12:13 +0100

    create and read

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const uuid = require("uuid");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.port || 3000;
 
 const welcomeMessage = {
   id: 0,
@@ -16,8 +21,25 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", (req, res) => res.json(messages));
+
+app.post("/messages", (req, res) => {
+  const newMessage = {
+    id: uuid.v4(),
+    from: req.body.from,
+    text: req.body.text,
+  };
+
+  if (!newMessage.from || !newMessage.text) {
+    res.status(400).json({ msg: "Please include a name and message" });
+  } else {
+    messages.push(newMessage);
+    res.json(messages);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
