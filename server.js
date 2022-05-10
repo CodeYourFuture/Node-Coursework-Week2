57f908f9f173c7dfa607556ed6dacc72896da93f
Timea <timea.reich@gmail.com>
2022-05-11 00:51:22 +0100

    Level 2 and Level 3

--- server.js
+++ server.js
@@ -18,11 +18,41 @@ const messages = [welcomeMessage];
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+
 app.get("/messages", (req, res) => {
   res.send(messages);
 });
+app.get("/messages/search", (req, res) => {
+  const searchTerm = req.query.text;
+  const foundMessage = messages.filter((item) =>
+    item.text.includes(searchTerm)
+  );
+  if (foundMessage) {
+    res.send(foundMessage);
+  } else {
+    res.send("No message found");
+  }
+});
+
+app.get("/messages/latest", (req, res) => {
+  if (messages.length < 10) {
+    res.send("There is less than 10 message");
+  } else {
+    res.send(messages.slice(-10));
+  }
+});
+
+app.get("/messages/:id", (req, res) => {
+  const id = req.params.id;
+  const foundMessage = messages.find((item) => item.id === Number(id));
+  res.send(foundMessage);
+});
+
 app.post("/messages", (req, res) => {
   const { from, text } = req.body;
+  if (from === "" || text === "") {
+    res.status(400).send("Please fill in the form");
+  }
   const newMessage = {
     id: messages.length,
     from: from,
