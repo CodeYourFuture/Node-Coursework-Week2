cb50755d48bc77aa54e68c0cb98705d7b9e89759
hachi-ops <elen20@protonmail.com>
2022-05-11 23:06:30 +0100

    update

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const res = require("express/lib/response");
 
 const app = express();
 
 app.use(cors());
-
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json());
+const PORT = process.env.port || 4100;
 
 const welcomeMessage = {
   id: 0,
@@ -19,40 +18,53 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-
-const PORT = process.env.PORT || 5050;
-
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-let data = [""];
+app.get("/messages", (req, res) => res.json(messages));
 
-app.post("/:name", function (req, res) {
-  const name = req.params.name;
-  data.push(name);
-  res.send(data);
+app.post("/messages", (req, res) => {
+  const newMessage = {
+    id: messages.length,
+    from: req.body.from,
+    text: req.body.text,
+  };
+
+  if (!newMessage.from || !newMessage.text) {
+    res.status(400).json({ msg: "Fill in the missing fields" });
+  } else {
+    messages.push(newMessage);
+    res.json(messages);
+  }
 });
 
-app.post("/name", function (req, res) {
-  // const name = req.body.name;
-  const { name } = req.body;
-  console.log(req.body);
-  data.push(name);
-  res.send(data);
+const findMessage = (id) =>
+  messages.find((message) => message.id === Number(id));
+
+app.get("/messages/:id", (req, res) => {
+  if (findMessage(req.params.id)) {
+    findMessage(req.params.id);
+  } else {
+    res
+      .status(400)
+      .json({ msg: `This message ${req.params.id}  does not exist` });
+  }
+  res.send(findMessage(req.params.id));
 });
 
-app.delete("/:name", (req, res) => {
-  data = data.filter((name) => name !== req.params.name);
-  res.send(data);
+app.delete("/messages/:id", (req, res) => {
+  const index = messages.indexOf(findMessage(req.params.id));
+  if (findMessage(req.params.id)) {
+    messages.splice(index, 1);
+  } else {
+    res.status(400).json({
+      msg: `Could not delete. The message with id: ${req.params.id} does not exist`,
+    });
+  }
+  res.send(messages);
 });
 
-// app.listen(process.env.PORT);
-
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
-
-// const listener = app.listen(process.env.PORT, function () {
-//   console.log("Your app is listening on port " + listener.address().port);
-// });
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
