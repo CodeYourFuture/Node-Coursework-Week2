60fa294e01d57f898f892620ac9ed4eb754952b6
lalehkoupa <lalehkoupa@gmail.com>
2022-05-12 11:05:35 +0100

    Level 4 completed

--- Messages.js
+++ Messages.js
@@ -1,7 +1,27 @@
 const messages = [
-  { id: 1, from: "Laleh", text: "hello this is Laleh" },
-  { id: 2, from: "Maryam", text: "hello this is Maryam" },
-  { id: 3, from: "Ali", text: "hello this is Ali" },
-  { id: 4, from: "Reza", text: "hello this is Reza" },
+  {
+    id: 1,
+    from: "Laleh",
+    text: "hello this is Laleh",
+    timeSent: `${new Date().getHours()} : ${new Date().getMinutes()} : ${new Date().getSeconds()} `,
+  },
+  {
+    id: 2,
+    from: "Maryam",
+    text: "hello this is Maryam",
+    timeSent: `${new Date().getHours()} : ${new Date().getMinutes()} : ${new Date().getSeconds()} `,
+  },
+  {
+    id: 3,
+    from: "Ali",
+    text: "hello this is Ali",
+    timeSent: `${new Date().getHours()} : ${new Date().getMinutes()} : ${new Date().getSeconds()} `,
+  },
+  {
+    id: 4,
+    from: "Reza",
+    text: "hello this is Reza",
+    timeSent: `${new Date().getHours()} : ${new Date().getMinutes()} : ${new Date().getSeconds()} `,
+  },
 ];
 module.exports = messages;
