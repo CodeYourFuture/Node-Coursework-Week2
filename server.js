90b423373b035c14742ae805a9aa909c612231d0
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-05-18 01:06:35 +0100

    managed to get the server to run on postman & browser - working on post method.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { response } = require("express");
+const { request } = require("express");
 
 const app = express();
 
@@ -15,9 +17,34 @@ const welcomeMessage = {
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
+console.log(messages);
+// console.log(messages.map(message => message.from));
+// console.log(messages.map(message => message.text));
+
+const PORT = 3000;
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", (request, response) => {
+  response.send(messages);
+})
+
+app.post("/messages", (request, response) => {
+
+})
+
+// const numberArray = [1, 2, 3, 4, 5];
+// console.log(Math.max(...numberArray));
+
+const idsAlreadyUsed = messages.map(message => message.id);
+// console.log(idsAlreadyUsed);
+const highestId = Math.max(...idsAlreadyUsed);
+// console.log(highestId)
+
+
+
+
+
+app.listen(PORT, console.log(`listening on ${PORT}`));
