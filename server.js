2999694622507b22d7bb0b19704d4757b8d15da2
s-shah-coder <saf.h.shah@gmail.com>
2022-05-19 14:22:41 +0100

    level 3 complete

--- server.js
+++ server.js
@@ -13,13 +13,17 @@ const welcomeMessage = {
 
 const messages = [welcomeMessage];
 
-app.get("/",  (req, res) => {
-  res.json("you found me!");
+// app.get("/",  (req, res) => {
+//   res.json("you found me!");
+// });
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get("message/thisClashes", (req, res) => {
-  res.json("theClashes");
-});
+// app.get("message/thisClashes", (req, res) => {
+//   res.json("theClashes");
+// });
 
 app.get("/message/messageId", (req, res) => {
   const { messageId } = req.params;
@@ -32,6 +36,30 @@ app.get("/message/messageId", (req, res) => {
   };
 });
 
+app.delete("/message/:messageId", (req, res) => {
+  const { messageId } = req.params;
+
+  const foundMessagesIndex = messages.findIndex((message) => message.id === Number(messageId));
+
+  if(foundMessageIndex > -1) {
+    res.json(messages[foundMessageIndex]);
+    messages.splice(foundMessageIndex, 1);
+  } else {
+    res.status(404).json(`couldn't find an item with the id ${messageId}`);
+  };
+});
+
+app.get("/messages/search", (req, res) => {
+  const { test } = req.query;
+
+  const filteredMessages = messages.filter((message) => message.text.includes(text));
+  res.json(filteredMessages);
+});
+
+app.get("/messages/latest", (req, res) => {
+  res.json(messages.slice(-10));
+});
+
 app.get("/messages", (req, res) => {
   const { id } = req.query;
   if (id !== undefined) {
@@ -40,6 +68,24 @@ app.get("/messages", (req, res) => {
     return res.json(messageToReturn);
   };
   res.json(messages);
+});
+
+app.post("/messages", (req, res) => {
+  const { from, text } = req.body;
+
+  if (from === undefined || from === "" || text === undefined || text === "") {
+    const helpfulMessage = {
+      msg: `wrong input for ${!from ? "from" : "text"}`,
+    };
+    return res.status(400).json(helpfulMessage);
+  }
+  const ourMessageObject = {
+    id: messages,length,
+    from,
+    text,
+  };
+  messages.push(ourMessageObject);
+  res.json("you've POST /messages")
 });
 
 
