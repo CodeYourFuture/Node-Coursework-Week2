9397abd3727a43f012ed3c7e390afd845492976e
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-05-23 01:30:35 +0100

    successfully implemented POST; also adding new id to each new message + able to load individual message by id

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const { response } = require("express");
-const { request } = require("express");
 
+const bodyParser = require("body-parser");
+const { request } = require("express");
+const { response } = require("express");
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const welcomeMessage = {
   id: 0,
@@ -17,10 +19,6 @@ const welcomeMessage = {
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
-console.log(messages);
-// console.log(messages.map(message => message.from));
-// console.log(messages.map(message => message.text));
-
 const PORT = 3000;
 
 app.get("/", function (request, response) {
@@ -31,20 +29,29 @@ app.get("/messages", (request, response) => {
   response.send(messages);
 })
 
-app.post("/messages", (request, response) => {
-
-})
-
-// const numberArray = [1, 2, 3, 4, 5];
-// console.log(Math.max(...numberArray));
-
+// keeps track of ids that have already been used
 const idsAlreadyUsed = messages.map(message => message.id);
-// console.log(idsAlreadyUsed);
-const highestId = Math.max(...idsAlreadyUsed);
-// console.log(highestId)
 
+app.post("/messages", (request, response) => {
+  console.log(request.body);
+  const newMessage = {
+    /*id function increments new ids based on 
+     highest id number contained within above array */
+    id: (Math.max(...idsAlreadyUsed) + 1),
+    from: request.body.from,
+    text: request.body.text,
+  }
+  messages.push(newMessage);
+  idsAlreadyUsed.push(newMessage.id);
+  console.log(idsAlreadyUsed);
+});
 
+app.get("/messages/:id", (request, response) => {
+  const message = messages
+    .find(message => message.id === Number(request.params.id));
 
+  response.send(message);
+})
 
 
 app.listen(PORT, console.log(`listening on ${PORT}`));
