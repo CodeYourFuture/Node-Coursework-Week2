33b065366ffa86023825c0c0771d843a5e72fece
Baz Murphy <bazmurphy@gmail.com>
2022-08-24 14:02:04 +0100

    level 4

--- server.js
+++ server.js
@@ -14,69 +14,88 @@ const messages = [
   {
     id: 0,
     from: "Bart",
-    text: "Welcome to CYF chat system!"
+    text: "Welcome to CYF chat system!",
+    timeSent: "2022-08-24T12:00:00.000Z",
   },
   {
     id: 1,
     from: "Baz",
-    text: "Hello Bart"
+    text: "Hello Bart",
+    timeSent: "2022-08-24T12:46:44.483Z",
   },
   {
     id: 2,
     from: "Bart",
-    text: "Hi Baz, How are you?"
+    text: "Hi Baz, How are you?",
+    timeSent: "2022-08-24T12:46:48.454Z",
   },
   {
     id: 3,
     from: "Baz",
-    text: "I'm ok, thank you"
+    text: "I'm ok, thank you",
+    timeSent: "2022-08-24T12:46:50.519Z",
   },
   {
     id: 4,
     from: "Bart",
-    text: "What are you doing today?"
+    text: "What are you doing today?",
+    timeSent: "2022-08-24T12:46:52.268Z",
   },
   {
     id: 5,
     from: "Baz",
-    text: "I am trying to learn Express!"
+    text: "I am trying to learn Express!",
+    timeSent: "2022-08-24T12:46:55.282Z",
   },
   {
     id: 6,
     from: "Bart",
-    text: "Well done"
+    text: "Well done",
+    timeSent: "2022-08-24T12:46:56.555Z",
   },
   {
     id: 7,
     from: "Baz",
-    text: "I just found out Express routing has specificity and that caused me a headache because i didn't know that before now!"
+    text: "I just found out Express routing has specificity and that caused me a headache because i didn't know that before now!",
+    timeSent: "2022-08-24T12:46:59.566Z",
   },
   {
     id: 8,
     from: "Bart",
-    text: "Yes, you need to declare the routes in specificity order to avoid issues. Or you can use more specific URL routes to help avoid issues with that."
+    text: "Yes, you need to declare the routes in specificity order to avoid issues. Or you can use more specific URL routes to help avoid issues with that.",
+    timeSent: "2022-08-24T12:47:01.115Z",
   },
   {
     id: 9,
     from: "Baz",
-    text: "I need 11 messages on here to be able to test the /messages/latest/ route!"
+    text: "I need 11 messages on here to be able to test the /messages/latest/ route!",
+    timeSent: "2022-08-24T12:47:03.428Z",
   },
   {
     id: 10,
     from: "Bart",
-    text: "Okay, let's keep talking then!"
+    text: "Okay, let's keep talking then!",
+    timeSent: "2022-08-24T12:47:06.417Z",
   },
   {
     id: 11,
     from: "Baz",
-    text: "I think this is the 11th message now :)"
+    text: "I think this is the 11th message now :)",
+    timeSent: "2022-08-24T12:47:09.245Z",
   },
   {
     id: 12,
     from: "Bart",
-    text: "Great, go try and make the route work, good luck!"
-  }
-]
+    text: "Great, go try and make the route work, good luck!",
+    timeSent: "2022-08-24T12:47:11.653Z",
+  },
+  {
+    id: 13,
+    from: "Baz",
+    text: "I just got timeSent working!",
+    timeSent: "2022-08-24T12:48:48.017Z",
+  },
+];
 
 app.get("/", function (req, res) {
   // console.log("GET / route")
@@ -97,7 +116,12 @@ app.get("/messages/search", function (req, res) {
   // console.log("GET /messages/search?text= route");
   const searchQuery = req.query.text;
   const searchResults = messages.filter(element => element.text.toLowerCase().includes(searchQuery.toLowerCase()));
-  res.send(searchResults);
+  if (searchResults.length) {
+    res.send(searchResults);
+  } else {
+    res.status(400);
+    res.send(`A message including the text ${req.query.text} does not exist`);
+  }
 });
 
 app.get("/messages/:id", function(req, res) {
@@ -132,6 +156,7 @@ app.post("/messages", function(req, res) {
       id: messages.length,
       from: req.body.from,
       text: req.body.text,
+      timeSent: new Date(),
     };
     messages.push(newMessage);
     res.redirect('/');
@@ -144,6 +169,7 @@ app.post("/messages", function(req, res) {
     } else if (!req.body.text) {
       res.send(`You didn't submit your Message`)
     }
+    // ^not sure how best to return these...
   }
 });
 
