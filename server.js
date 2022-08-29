8345eefadf73c4e629c449e6d695eed2d643e3ff
Baz Murphy <bazmurphy@gmail.com>
2022-08-29 15:46:16 +0100

    minor adjustments

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 const cors = require("cors");
 
 app.use(cors());
@@ -10,92 +10,8 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-const messages = [
-  {
-    id: 0,
-    from: "Bart",
-    text: "Welcome to CYF chat system!",
-    timeSent: "2022-08-24T12:00:00.000Z",
-  },
-  {
-    id: 1,
-    from: "Baz",
-    text: "Hello Bart",
-    timeSent: "2022-08-24T12:46:44.483Z",
-  },
-  {
-    id: 2,
-    from: "Bart",
-    text: "Hi Baz, How are you?",
-    timeSent: "2022-08-24T12:46:48.454Z",
-  },
-  {
-    id: 3,
-    from: "Baz",
-    text: "I'm ok, thank you",
-    timeSent: "2022-08-24T12:46:50.519Z",
-  },
-  {
-    id: 4,
-    from: "Bart",
-    text: "What are you doing today?",
-    timeSent: "2022-08-24T12:46:52.268Z",
-  },
-  {
-    id: 5,
-    from: "Baz",
-    text: "I am trying to learn Express!",
-    timeSent: "2022-08-24T12:46:55.282Z",
-  },
-  {
-    id: 6,
-    from: "Bart",
-    text: "Well done",
-    timeSent: "2022-08-24T12:46:56.555Z",
-  },
-  {
-    id: 7,
-    from: "Baz",
-    text: "I just found out Express routing has specificity and that caused me a headache because i didn't know that before now!",
-    timeSent: "2022-08-24T12:46:59.566Z",
-  },
-  {
-    id: 8,
-    from: "Bart",
-    text: "Yes, you need to declare the routes in specificity order to avoid issues. Or you can use more specific URL routes to help avoid issues with that.",
-    timeSent: "2022-08-24T12:47:01.115Z",
-  },
-  {
-    id: 9,
-    from: "Baz",
-    text: "I need 11 messages on here to be able to test the /messages/latest/ route!",
-    timeSent: "2022-08-24T12:47:03.428Z",
-  },
-  {
-    id: 10,
-    from: "Bart",
-    text: "Okay, let's keep talking then!",
-    timeSent: "2022-08-24T12:47:06.417Z",
-  },
-  {
-    id: 11,
-    from: "Baz",
-    text: "I think this is the 11th message now :)",
-    timeSent: "2022-08-24T12:47:09.245Z",
-  },
-  {
-    id: 12,
-    from: "Bart",
-    text: "Great, go try and make the route work, good luck!",
-    timeSent: "2022-08-24T12:47:11.653Z",
-  },
-  {
-    id: 13,
-    from: "Baz",
-    text: "I just got timeSent working!",
-    timeSent: "2022-08-24T12:48:48.017Z",
-  },
-];
+const messages = require("./messages.json");
+// console.log(messages);
 
 app.get("/", function (req, res) {
   // console.log("GET / route")
@@ -108,6 +24,7 @@ app.get("/messages", function(req, res) {
 });
 
 app.get("/messages/latest", function (req, res) {
+  // console.log("GET /messages/latest route")
   const lastTenMessages = messages.slice(messages.length - 10);
   res.send(lastTenMessages);
 });
@@ -129,8 +46,8 @@ app.get("/messages/:id", function(req, res) {
 
   // I SPENT WAY TOO LONG WORKING THIS OUT!!
   // WE HAVE TO TYPE CONVERT THE STRING REQUEST TO NUMBER FOR ID IN THE OBJECT
-  console.log(`req.params.id : ${req.params.id}`);
-  console.log(`req.params.id is type ${typeof req.params.id}`)
+  // console.log(`req.params.id : ${req.params.id}`);
+  // console.log(`req.params.id is type ${typeof req.params.id}`)
   // console.log(`messages[0].id : ${messages[0].id}`)
   // console.log(`messages[0].id is type ${typeof messages[0].id}`)
   
@@ -170,6 +87,30 @@ app.post("/messages", function(req, res) {
       res.send(`You didn't submit your Message`)
     }
     // ^not sure how best to return these...
+  }
+});
+
+app.put("/messages/:id", function(req,res) {
+  console.log(`req.params.id ${req.params.id}`);
+  console.log(`req.body ${req.body}`)
+  const indexOfMessageId = messages.findIndex(element => element.id === Number(req.params.id));
+  // console.log(indexOfMessageId);
+  console.log(messages[indexOfMessageId]);
+  if (indexOfMessageId) {
+    if (req.body.from && req.body.text) {
+      messages[indexOfMessageId].from = req.body.from;
+      messages[indexOfMessageId].text = req.body.text;
+      res.send(`Updated message with id ${req.params.id} with Name ${req.body.from} and with Message ${req.body.text}`)
+    } else if (req.body.from) {
+      messages[indexOfMessageId].from = req.body.from;
+      res.send(`Updated message with id ${req.params.id} with Name ${req.body.from}`)
+    } else if (req.body.text){
+      messages[indexOfMessageId].text = req.body.text;
+      res.send(`Updated message with id ${req.params.id} with Message ${req.body.text}`)
+    }
+  } else {
+    res.status(400);
+    res.send(`A message with the id ${req.params.id} does not exist to update`);
   }
 });
 
