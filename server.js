3b41dc3373cf3e53c4b5cba649e80ec42d592604
ChrisKoetzee <102851549+ChrisKoetzee@users.noreply.github.com>
2022-11-16 11:49:54 +0200

    first commit

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.json())
+
 const welcomeMessage = {
   id: 0,
   from: "Bart",
@@ -20,4 +22,44 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.post('/new/:id/:name/:text', (req, res)=>{
+  let id = req.params.id;
+  let name = req.params.name;
+  let text = req.params.text;
+  console.log(req.body.messages = JSON.stringify(id, name, text));
+  res.sendStatus(200);
+});
+
+app.get('/getAll', (req, res) => res.send(messages.JSON));
+
+app.post('/post', (req, res)=>{
+  console.log(req.body.messages)
+  console.log(`These are all my messages before pushing : ${messages.length}`);
+  messages.push(req.body.messages.JSON.toString);
+  console.log(`These are all my messages after pushing : ${messages.length}`);
+  res.sendStatus(200);
+});
+
+app.get('/message/:id', (req, res) => {
+  const id = req.params.id;
+  let filteredMessage = messages.filter(item => item.id ==! id);
+  console.log(filteredMessage.length);
+res.sendStatus(400).json.toString({success: true});
+});
+
+app.delete('/delete/:id', (req, res) =>{
+  console.log('Delete/delete route');
+  const paramsId = req.params.id;
+  let messageFiltered = messages.filter(item => item.id == paramsId);
+  console.log(messageFiltered);
+  res.sendStatus(200).json.toString({success: true});
+  })
+
+
+app.listen(3001);
+
+// app.post('/index/:message', (req, res)=>{
+//   let id = req.params.id;
+//   let filteredMessage = messages.filter(mess => mess.id == id);
+//   res.send(filteredMessage ==! id || "" ? res.sendStatus(400) : console.log(req.body) );
+// });
