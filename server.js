92079287ba6c1e2a520830a3f62c3d6149ff0d34
Riyaaz Singh <riyaazct1@gmail.com>
2022-11-17 21:32:37 +0200

    aupdate to level 1

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [];
+const messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
@@ -30,11 +30,12 @@ app.get("/messages", (req, res) => {
   res.json(messages);
 });
 
+// Send a message
 app.post("/messages", (req, res) => {
   console.log(req.body);
   const newMessage = {
-    /*id: uuid.v4()*/ //to be used in the generating of random id but for now i'm using the below method for ease of access
-    id: messages.length + 1,
+    /*id: uuid.v4(),*/ //to be used in the generating of random id but for now i'm using the below method for ease of access
+    id: messages.length,
     from: req.body.from,
     text: req.body.text,
   };
@@ -48,6 +49,15 @@ app.post("/messages", (req, res) => {
 
   messages.push(newMessage);
   res.json(messages);
+});
+
+app.get("/messages/:id", (req, res) => {
+  const inputId = parseInt(req.params.id);
+  let messageReturned = messages.filter(
+    (message) => message.id === inputId
+  );
+  console.log(messageReturned);
+  res.send(messageReturned);
 });
 
 const port = 3001;
