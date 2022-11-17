bcd2213e2bd9d224a2f598fa24346c669def6c38
Riyaaz Singh <riyaazct1@gmail.com>
2022-11-17 23:33:31 +0200

    complete level 1

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
@@ -32,7 +32,6 @@ app.get("/messages", (req, res) => {
 
 // Send a message
 app.post("/messages", (req, res) => {
-  console.log(req.body);
   const newMessage = {
     /*id: uuid.v4(),*/ //to be used in the generating of random id but for now i'm using the below method for ease of access
     id: messages.length,
@@ -51,13 +50,39 @@ app.post("/messages", (req, res) => {
   res.json(messages);
 });
 
+// Get one message by ID
 app.get("/messages/:id", (req, res) => {
   const inputId = parseInt(req.params.id);
-  let messageReturned = messages.filter(
-    (message) => message.id === inputId
-  );
-  console.log(messageReturned);
-  res.send(messageReturned);
+  const found = messages.some((message) => message.id === inputId);
+
+  if (found) {
+    let messageReturned = messages.filter(
+      (message) => message.id === inputId
+    );
+
+    res.send(messageReturned);
+  } else {
+    res
+      .status(400)
+      .json({ Msg: `No message with id ${inputId} found` });
+  }
+});
+// Delete a message
+app.delete("/messages/:id", (req, res) => {
+  const inputId = parseInt(req.params.id);
+  const found = messages.some((message) => message.id === inputId);
+  if (found) {
+    res.json({
+      Msg: "message deleted",
+      messages: (messages = messages.filter(
+        (message) => message.id !== inputId
+      )),
+    });
+  } else {
+    res.status(400).json({
+      Msg: "Message with id " + req.params.id + " not found",
+    });
+  }
 });
 
 const port = 3001;
