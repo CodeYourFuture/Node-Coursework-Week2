c9b7da7d4721c52118c878c18b315c89d6668419
Nishka Kisten <nishka.crypto@gmail.com>
2022-11-19 00:01:06 +0200

    Level 1

--- server.js
+++ server.js
@@ -18,6 +18,35 @@ const messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
+  console.log(__dirname + "/index.html");
+});
+app.post("/messages", function (req, res) {
+  console.log("POST /messages routes");
+  console.log(req.body);
+
+  messages.push(req.body);
+  res.sendStatus(200);
+});
+
+app.get("/messages", function (req, res){
+ res.send(messages);
+})
+
+app.get("/messages/:id", (req, res) => {
+ const id = Number(req.params.id);
+const requiredMessage = messages.find(message => message.id === id);
+res.send(requiredMessage);
+})
+app.delete('/messages/:id', (req, res) => {
+ // const toDelete = parseInt(req.params.id);
+  const del = messages.filter(({ id }) => id !== req.params.id);
+  //  const notDeleted = messages.filter(message => message.id !== toDelete);
+    messages = del;
+  res.status(200).json({success: true});
 });
 
 app.listen(process.env.PORT);
+
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
