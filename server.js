2d1dbeea1247ee7acd5ea93750eed2ac3f57a3a7
Lihle Naphakade <31651036+LN2410@users.noreply.github.com>
2022-11-21 22:17:35 +0200

    Chat server exercise
    - Create a new message
    - Read all messages
    - Read one message specified by an ID
    - Delete a message, by ID

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.urlencoded());
 
 const welcomeMessage = {
   id: 0,
@@ -14,10 +15,35 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.post("/messages", function (request, response) {
+  messages.push({ id: messages.length, ...request.body });
+  response.status(200).redirect("/messages");
+});
+
+app.get("/messages", function (request, response) {
+  response.json(messages);
+});
+
+app.get("/messages/:id", function (request, response) {
+  const id = request.params.id;
+  response.json(messages[id]);
+});
+
+app.get("/messages/delete/:id", function (request, response) {
+  const id = request.params.id;
+  console.log(typeof id);
+  messages = messages.filter((_, index) => {
+    return index != id;
+  });
+  response.redirect("/messages");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on ${process.env.PORT}`);
+});
