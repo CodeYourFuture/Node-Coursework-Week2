2a3ac5e60b2841c215a1de7dd15cbced98fe1479
jaspreet kaur <kaurjas1928@yahoo.com>
2022-12-19 19:09:14 +0000

    created messages

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
+app.use(express.json());
 
+const cors = require("cors");
 app.use(cors());
+
+app.use(express.urlencoded({extended: false}))
+
+app.get("/", function(request, response){
+  response.sendFile(__dirname + "/index.html");
+});
 
 const welcomeMessage = {
   id: 0,
@@ -11,13 +17,28 @@ const welcomeMessage = {
   text: "Welcome to CYF chat system!",
 };
 
+const myMessage = {
+  id: 1,
+  from: "Jas",
+  text: "Hello, how are you?",
+};
+
+const joannaMessage = {
+  id: 0,
+  from: "Joanna",
+  text: "Have a lovely day",
+};
+
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+const messages = [welcomeMessage, myMessage, joannaMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
-});
 
-app.listen(process.env.PORT);
+
+app.get('/messages', (req, res)=> {
+  res.send(messages);
+} );
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server on port 3000'));
