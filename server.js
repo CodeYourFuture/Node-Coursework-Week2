87ded8394afdc8028032efdb674db1483ff7c41b
Dawit Abraha <akdawit1@gmail.com>
2022-12-21 22:59:14 +0000

    Completed level 4

--- server.js
+++ server.js
@@ -3,21 +3,76 @@ const cors = require("cors");
 
 const app = express();
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
 app.use(cors());
 
 const welcomeMessage = {
   id: 0,
+  timeSent: new Date().toLocaleDateString(),
   from: "Bart",
   text: "Welcome to CYF chat system!",
 };
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [welcomeMessage];
+let incrementId = 1;
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.get("/messages/latest/", (req, res) => {
+  let latestMsg = messages.filter(
+    (messages, index) => messages.length - 10 <= index
+  );
+  res.send(latestMsg);
+});
+
+app.get("/messages/:id", (req, res) => {
+  let foundMsg = messages.find((msg) => msg.id == req.params.id);
+  res.send(foundMsg);
+});
+
+app.post("/messages", (req, res) => {
+  const newMessage = {
+    id: incrementId,
+    timeSent: new Date().toLocaleDateString(),
+    from: req.body.from,
+    text: req.body.text,
+  };
+  incrementId += 1;
+  messages.push(newMessage);
+  res.send(messages);
+});
+
+app.delete("/messages/:id", (req, res) => {
+  const selectedId = req.params.id;
+  const found = messages.some((message) => message.id == selectedId);
+  if (found) {
+    messages = messages.filter((message) => message.id != selectedId);
+    res.status(204).json({ msg: `Message has been deleted` });
+  } else {
+    res.status(400).json({ msg: `No message with the id of ${selectedId}` });
+  }
+});
+
+app.put("/messages/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const message = messages.find((msg) => id === msg.id);
+  if (message) {
+    let query = req.body;
+    message.text = query.text;
+    message.from = query.from;
+    res.json(message);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.listen(process.env.PORT || 7000);
