bc749811bd2dd8b0322ac68d58e1ae0ae0ba17d2
simeonbikov <bikovsem@gmail.com>
2022-12-21 23:45:18 +0000

    level-1 completed

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
@@ -11,13 +12,57 @@ const welcomeMessage = {
   text: "Welcome to CYF chat system!",
 };
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.post("/messages", (request, response) => {
+  if (!request.body.from || !request.body.text) {
+    response.sendStatus(400);
+    return;
+  }
+  const createdMessage = {
+    id: Number(new Date()),
+    from: request.body.from,
+    text: request.body.text,
+  };
+
+  messages.push(createdMessage);
+  console.log(createdMessage);
+  response.status(201).json(createdMessage);
+});
+
+app.get("/messages", function (request, response) {
+  response.json(messages);
+});
+
+app.get("/messages/:id", (request, response) => {
+  const foundMessage = messages.find(
+    (message) => message.id === Number(request.params.id)
+  );
+  if (!foundMessage) {
+    response.sendStatus(404);
+    return;
+  }
+  response.json(foundMessage);
+});
+
+app.delete("/messages/:id", (request, response) => {
+  const delMessageId = Number(request.params.id);
+  const foundMessageId = messages.findIndex(
+    (message) => message.id === delMessageId
+  );
+
+  if (foundMessageId < 0) {
+    response.sendStatus(404);
+    return;
+  }
+
+  messages.splice(foundMessageId, 1);
+  response.sendStatus(204);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port: ${port}`));
