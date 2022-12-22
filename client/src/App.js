3acdb2a7dc5d7e1f84d9af7541e661df58e87106
JanefrancessC <janefrancesschioma@gmail.com>
2022-12-22 19:01:25 +0000

    created full stack

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import './App.css';
+import SendMessage from './components/SendMessage';
+
+const App = () => {
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    fetch("/messages")
+    .then((res) => res.json())
+    .then((data) => setMessages(data))
+    .catch((error) => console.error(error))
+  }, [])
+  return (
+    <div className="App">
+      <SendMessage messages={messages} setMessages={setMessages} />
+      {messages.map((msg, index) => {
+        return (
+          <div>
+            <p key={index}>{msg.text}</p>
+            <p >From: {msg.from}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const cors = require("cors");
-
+// const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 const app = express();
-
-app.use(cors());
+const fs = require("fs");
+let data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+//app.use(cors());
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
@@ -16,8 +18,32 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+// app.get("/", function (request, response) {
+//   response.json(welcomeMessage).sendFile(__dirname + "/index.html");
+
+// });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server is ready" });
+});
+app.get("/messages", (req, res) => {
+  res.json(data);
+});
+app.post("/messages", (req, res) => {
+  if (!req.body.from || !req.body.text) {
+    res.status(400).json({msg: "Please fill all fields"});
+    return;
+  }
+  let newID = Math.max(...data.map((msg) => msg.id)) + 1;
+  let newMessage = { id: newID, from: req.body.from, text: req.body.text };
+  data.push(newMessage);
+  save();
+  res.json(newMessage);
 });
 
-app.listen(process.env.PORT);
+const save = () => {
+  fs.writeFileSync("data.json", JSON.stringify(data, null, 2));
+};
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
