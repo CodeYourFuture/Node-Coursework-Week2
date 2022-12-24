5cc2a8bf39cbf0694e1091ee49c8e9ca22fe9be1
KawaManmi <kawamam2007@gmail.com>
2022-12-24 13:04:05 +0000

    Create new message has been done

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const cors = require("cors");
 
 const app = express();
 
+//  app.use(express.json())
+
+const bp = require('body-parser');
+const { response } = require("express");
+
+app.use(bp.json());
+app.use(bp.urlencoded({extended:true}));
+
 app.use(cors());
 
 const welcomeMessage = {
@@ -16,8 +24,37 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
+
+let maxID = Math.max(...messages.map(c => c.id))
+
+app.get("/", function (request, response, next) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+// app.get("/messages", function (request, response) {
+//   response.send(welcomeMessage);
+// });
+
+
+app.post('/messages', (req, res,next) => {
+  if (!req.body.from || !req.body.text){   
+    res.status(400).send(` You must include a from and a and text in your request`)
+    return
+  } 
+  else{
+    // res.send(req.body)
+    const newMessage = {
+      "id": ++maxID,
+      "from": req.body.from,
+      "text": req.body.text
+    }
+    messages.push(newMessage)
+      res.send(`<h1>The message has been added</h1> <button onclick="history.back()"> Go Back </button> `)
+    
+  }
+
+});
+
+// app.listen(process.env.PORT);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
