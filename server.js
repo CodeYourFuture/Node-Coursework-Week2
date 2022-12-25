39406d4ef422d5a39aed2fd66921adfef3e2622f
irina mihai <irinamihai150@gmail.com>
2022-12-25 21:11:57 +0000

    level1

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
+const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
 const welcomeMessage = {
-  id: 0,
+  id: "0",
   from: "Bart",
   text: "Welcome to CYF chat system!",
 };
@@ -19,5 +20,48 @@ const messages = [welcomeMessage];
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+app.post("/messages", (req, res) => {
+  //validate our input
+  if (!req.body.from || !req.body.text) {
+    res.status(400).send("from must be valid");
+    return;
+  }
 
-app.listen(process.env.PORT);
+  // build our new resourse
+  const newMessage = req.body;
+  newMessage.id = uuidv4();
+  messages.push(newMessage);
+  res.status(201).json(newMessage);
+});
+app.get("/messages", (req, res) => {
+  res.json(messages);
+});
+
+app.get("/messages/:id", (req, res) => {
+  console.log(req.params);
+  const id = req.params.id;
+  const msg = messages.find((message) => message.id === id);
+  if (msg === undefined) {
+    res.send("message not found");
+  }
+
+  res.send(msg);
+});
+app.delete("/messages/:id", (req, res) => {
+  // validate your input
+  const id = req.params.id;
+  const msg = messages.find((message) => message.id === id);
+  if (msg === undefined) {
+    res.status(404).send("message not found");
+  }
+  const messageIndex = messages.findIndex((m) => m.id === id);
+  messages.splice(messageIndex, 1);
+  res.send("message deleted");
+  res.send("message deleted");
+});
+const port = 3131;
+
+app.listen(port, () => {
+  console.log("http://localhost:" + port);
+});
+// app.listen(process.env.PORT);
