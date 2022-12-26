2b3d7437778cfe443d6694b3ed5577ba54bbe918
jaspreet kaur <kaurjas1928@yahoo.com>
2022-12-26 23:22:06 +0000

    try to add new messages

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", function(request, response){
+app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
@@ -34,11 +34,26 @@ const joannaMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage, myMessage, joannaMessage];
 
+app.post("/messages", (req, res) => {
+  const { from, text } = req.body;
 
+  const newMessage = {
+    id: messages.length,
+    from,
+    text,
+    timeStamp: (TimeDate=new Date()),
+  };
 
-app.get('/messages', (req, res)=> {
+  if(!newMessage.from || !newMessage.text){
+    return res.status(400).json("Please include a name and message");
+  }
+  messages.push(newMessage);
+  res.send(messages)
+});
+
+app.get("/messages", (req, res) => {
   res.send(messages);
-} );
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server on port 3000'));
+app.listen(PORT, () => console.log("Server on port 3000"));
