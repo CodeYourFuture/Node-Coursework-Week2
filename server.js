34e253fcd2b2474eecb0c4da5e4195a7db750e5e
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2022-12-28 12:50:47 +0000

    bodyParserAdded

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
@@ -10,6 +13,7 @@ const welcomeMessage = {
   id: 0,
   from: "Joanna",
   text: "Welcome to CYF chat system!",
+  timeSent: 28 / 12 / 2022,
 };
 
 //This array is our "data store".
@@ -17,35 +21,46 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/",  (req, res) => {
+app.get("/messages/search", (req, res) => {
+  const { term } = req.query;
+  console.log(term);
+
+  const filterMessages = messages.filter((message) =>
+    message.text.toLocaleLowerCase().includes(term.toLocaleLowerCase())
+  );
+  console.log(filterMessages);
+  res.send(filterMessages);
+});
+
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 //All messages
 app.get("/messages", (req, res) => {
-  res.send(messages);
+  res.json(messages);
+});
+
+//Last 10 messages
+
+app.get("/messages/latest", (req, res) => {
+  res.send(messages.slice(-10))
 })
 
 //New message
 app.post("/messages", (req, res) => {
   const { from, text } = req.body;
 
-  const newMessages = {
+  const messageObject = {
     id: messages.length + 1,
     from,
     text,
+    timeSent: new Date().toLocaleDateString(),
   };
-
-if (!newMessages.from || !newMessages.text) {
-  return res.status(400).json("You must include a name and message");
-}
-
-messages.push(newMessages);
-res.send(messages);
-})
+});
 
 //Message specified by ID
-app.get("/messages/:id", (req,res) => {
+app.get("/messages/:id", (req, res) => {
   const foundId = messages.filter((i) => i.id === Number(req.params.id));
 
   if (foundId) {
@@ -57,9 +72,7 @@ app.get("/messages/:id", (req,res) => {
 app.delete("/messages/:id", (req, res) => {
   const foundId = messages.filter((i) => i.id === Number(req.params.id));
 
-
-
-  if(foundId) {
+  if (foundId) {
     return res.status(200).json({
       msg: `Message id: ${req.params.id} deleted`,
       "All messages": messages.filter((i) => i.id !== Number(req.params.id)),
@@ -68,5 +81,3 @@ app.delete("/messages/:id", (req, res) => {
 });
 
 app.listen(PORT);
-
-
