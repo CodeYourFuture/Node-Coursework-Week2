7295808aedfbc2dd2d22e82eba07a58f5d2ddb32
Dawit Abraha <akdawit1@gmail.com>
2022-12-28 23:27:12 +0000

    Modified

--- client/cyf-chat/src/components/AllMessages.js
+++ client/cyf-chat/src/components/AllMessages.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import DeleteMessage from "./buttons/DeleteMessage";
 import EditMessage from "./buttons/EditMessage";
 // import AllMessages from './buttons/AllMessages';
