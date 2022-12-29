829ead2a568d9f87faee7e9c2c34dadfbf70c938
mesgna woldeb <mesgna.mehari8@gmail.com>
2022-12-29 00:44:57 +0000

    server side excerise done

--- server.js
+++ server.js
@@ -2,22 +2,93 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const port = process.env.PORT || 5000;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
 app.use(cors());
 
 const welcomeMessage = {
   id: 0,
+  timeSent: new Date().toLocaleDateString(),
   from: "Bart",
   text: "Welcome to CYF chat system!",
 };
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [welcomeMessage];
+let newID = 1;
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+app.get("/messages/search", (req, res) => {
+  const search = req.query.term;
+  const result = messages.filter(
+    (message) =>
+      message.from.toLowerCase().includes(search.toLowerCase()) ||
+      message.text.toLowerCase().includes(search.toLowerCase())
+  );
+  res.send(result);
+});
+app.get("/messages/latest/", (req, res) => {
+  let latestMsg = messages.filter(
+    (messages, index) => messages.length - 10 <= index
+  );
+  res.send(latestMsg);
+});
+
+app.get("/messages/:id", (req, res) => {
+  let foundMsg = messages.find((msg) => msg.id == req.params.id);
+  res.send(foundMsg);
+});
+
+app.post("/messages", (req, res) => {
+  if (!req.body.from || !req.body.text) {
+    res.status(400).json({ msg: "Please make sure to include text and from" });
+    return;
+  }
+
+  const newMessage = {
+    id: newID,
+    timeSent: new Date().toLocaleDateString(),
+    from: req.body.from,
+    text: req.body.text,
+  };
+  newID += 1;
+  messages.push(newMessage);
+  res.json(messages);
+});
+
+app.delete("/messages/:id", (req, res) => {
+  const selectedId = req.params.id;
+  const found = messages.some((message) => message.id == selectedId);
+  if (found) {
+    messages = messages.filter((message) => message.id != selectedId);
+    res.status(204).json({ msg: `Message has been deleted` });
+  } else {
+    res.status(400).json({ msg: `No message with the id of ${selectedId}` });
+  }
+});
+
+app.put("/messages/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const message = messages.find((msg) => id === msg.id);
+  if (message) {
+    let query = req.body;
+    message.from = query.from;
+    message.text = query.text;
+    res.json(message);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
