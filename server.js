d743038bc8d31487426d4fc86aa93321fd563266
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2022-12-29 10:31:16 +0000

    Level1Started

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-
+const uuid = require("uuid");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5000;
 const app = express();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(cors());
 
@@ -20,4 +23,33 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+//All messages
+app.get("/messages", function (request, response) {
+  response.status(200).json(messages);
+});
+
+//One message by ID
+app.get("/messages/:id", function (request, response) {
+  let message = messages.find((msg) => msg.id === request.params.id);
+  if (message) {
+    response.status(200).json(message);
+  } else {
+    response.status(400).json({
+      msg: "Message with an ID " + request.params.id + " doesn't exist",
+    });
+  }
+});
+
+//New message
+app.post("/messages", urlencodedParser, function (request, response) {
+  const newMessage = {
+    id: uuid.v4(),
+    from: request.body.from,
+    text: request.body.text,
+  };
+
+  messages.push(newMessage);
+  response.send(messages);
+});
+
+app.listen(PORT);
