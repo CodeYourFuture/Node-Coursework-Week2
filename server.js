46dd1ebd9f84655151ab60cfc2d72bf9bb9b3d66
Jade Jones <jadejonescrypto@gmail.com>
2023-01-02 21:17:03 +0000

    post not working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { request } = require("express");
 
 const app = express();
 
@@ -12,18 +13,38 @@ const welcomeMessage = {
   text: "Welcome to CYF chat system!",
 };
 
+const myMessage = {
+  id: 1,
+  from: "Jade",
+  text: "test"
+}
+
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+const messages = [welcomeMessage, myMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
-
+let maxID = Math.max(...messages.map(c => c.id));
 // create a message
-app.post("/messages", (req,res) => {
+app.post("/messages",  (req, res) => {
+  if (!req.body.from && !req.body.text){   
+    res.status(400).send("You must include a name and text in your request")
+  } 
+else{
+  const { from, text } = req.body;
+  let newMessage = {
+    id: ++maxID,
+    from,
+    text
+  }
 
+  messages.push(newMessage)
+  res.send(newMessage)
+}
+ 
 })
 
 // read all messages
@@ -49,8 +70,20 @@ app.get("/messages:id", (req, res) => {
 })
 // delete a message specified by id
 app.delete("/messages:id", (req,res) => {
+  messages.deleteOne({_id: req.params.id})
+  .then(() =>{
+    res.status(200).json({
+      message: "message deleted!",
+    });
+  })
+  .catch((error) =>{
+    res.status(400).json({
+      error: "error",
+    });
+  });
+});
 
-})
+
 
 const port = process.env.PORT || 4200;
 app.listen(port, () =>{
