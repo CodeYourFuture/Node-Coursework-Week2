14f28e619ff6c43bd28774911f4e202a81e6b0dc
Dawit Abraha <akdawit1@gmail.com>
2023-01-03 17:19:46 +0000

    Added path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 7000;
@@ -9,6 +10,7 @@ const port = process.env.PORT || 7000;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.resolve(__dirname, "./client/cyf-chat/build")));
 
 const welcomeMessage = {
   id: 0,
