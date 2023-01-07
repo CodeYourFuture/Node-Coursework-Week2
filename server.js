8465a0427b4a04eca39a0d7d1f3b45a3eadb667c
JanefrancessC <janefrancesschioma@gmail.com>
2023-01-07 19:24:48 +0000

    dependencies updated

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const app = express();
 const fs = require("fs");
 let data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 const welcomeMessage = {
