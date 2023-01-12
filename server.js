1f20a7585dac38e49cc11d622c3ba0457f617475
Jihad Omer <jhjahony@gmail.com>
2023-01-12 20:49:53 +0000

    Create a new Message

--- server.js
+++ server.js
@@ -14,10 +14,36 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
+
 const messages = [welcomeMessage];
+
+
+
+app.get("/messages", function (request, response) {
+  response.json(messages);
+});
+
+
+
+
+
+
+// function CreatNewMessage(messages) {
+//   return messages;
+// }
+
+
+
+
+// app.post("/", function (req, res) {
+//   res.send({messages}).json;
+// });
+
+//////////////////////////////////////////////////
+
+app.listen(process.env.PORT,()=>{console.log('server is listening');});
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
-});
+});  
 
-app.listen(process.env.PORT);
