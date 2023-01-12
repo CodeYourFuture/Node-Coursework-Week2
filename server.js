eb57eb831a1f3bb9fad6178d51ebcb462954f2da
Jihad Omer <jhjahony@gmail.com>
2023-01-12 21:17:21 +0000

    Read all

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const app = express();
 
+
 app.use(cors());
 
 const welcomeMessage = {
@@ -16,34 +17,37 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 
 const messages = [welcomeMessage];
+// Create a new message
 
+app.post("/messages", function (request, response) {
+  response.send({messages}).json;
+});
 
+//read all messages
+app.get("/readMessages", function (request, response) {
+  response.send({messages}).json;
+});
 
-app.get("/messages", function (request, response) {
-  response.json(messages);
+//read one message specified by an ID
+app.get("/readMessages/:id", function (request, response) {
+  const id = Number(request.params.id);
+  const message = messages.find((message) => message.id === id);
+  response.send(message);
 });
 
 
+//delete a message, by ID
+app.delete("/deleteMessages/:id", function (request, response) {
+  const id = Number(request.params.id);
+  const message = messages.find((message) => message.id === id);
 
-
-
-
-// function CreatNewMessage(messages) {
-//   return messages;
-// }
-
-
-
-
-// app.post("/", function (req, res) {
-//   res.send({messages}).json;
-// });
-
+});
 //////////////////////////////////////////////////
 
-app.listen(process.env.PORT,()=>{console.log('server is listening');});
+app.listen(process.env.PORT, () => {
+  console.log("server is listening");
+});
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
-});  
-
+});
