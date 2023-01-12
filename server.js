08131675330e1d1d9ef970b2d8938dd55e2da327
michelle <mindyourphotosynthesis@gmail.com>
2023-01-12 15:27:18 +0000

    coursework: updated

--- server.js
+++ server.js
@@ -12,23 +12,14 @@ const welcomeMessage = {
   text: 'Welcome to CYF chat system!',
 }
 
-let props = ['id', 'from', 'text']
-
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage]
-console.log(messages)
-// let nextId =
-//   1 +
-//   messages.reduce(
-//     (n, message) => (n < messages.message ? messages.message : n),
-//     0
-//   )
 
 app.get('/', function (request, response) {
   response.sendFile(__dirname + '/index.html')
-  //response.json(messages)
+  response.json(messages)
 })
 
 app.get('/messages', (request, response) => {
@@ -36,39 +27,33 @@ app.get('/messages', (request, response) => {
 })
 
 app.get(`/messages/:id`, (request, response) => {
-  //create a variable to store id in, parseInt
-  //if no var response.status(404).send('message)
-  //otherwise response.send(variable finding id)
-
   let req = request.params.id
-  let validator = messages.find((id) => messages[id] === req)
-
-  if (!validator) {
+  let validator =
+    messages.find((id) => messages[id] === req)(validator === undefined) &&
     response.status(404).send('Not Found')
-  }
 
   response.json(validator)
 })
 
 app.post('/messages', (request, response) => {
-  //create a request body
-  if (!props.every((prop) => request.body.hasOwnProperty(prop))) {
-    response.status(401).send('Please fill in all fields')
-  }
-
-  const newMessage = { id: messages.length }
-  props.forEach((prop) => {
-    if (request.body[prop]) {
-      newMessage[prop] = request.body[prop]
-    }
-  })
+  const newMessage = request.body
+  console.log(request.body)
+  newMessage.id = messages.length + 1
   messages.push(newMessage)
   response.json(messages)
 })
 
 app.delete(`/messages/:id`, (request, response) => {
   console.log(response)
+  const req = request.params.id
+  let validator =
+    messages.find((id) => messages[id] === req)(validator === undefined) &&
+    response.status(404).send('Not Found')
+
+  const msgIndex = messages.findIndex((ind) => ind === messages.req)
+  messages.splice(msgIndex, 1)
+  response.send('Message deleted')
 })
 
-app.listen(3000)
-//app.listen(process.env.PORT)
+//app.listen(3000)
+app.listen(process.env.PORT)
