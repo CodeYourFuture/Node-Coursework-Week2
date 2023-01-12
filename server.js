4b42bd8c5e4dba62148d4cf1ab34ddf9ad277a33
4maziar <53979524+8maziar@users.noreply.github.com>
2023-01-12 18:55:58 +0000

    level 1 & 2 & 3 fiinished

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
@@ -14,10 +15,51 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.get("/messages/search", (req, res) => {
+  const keyword = req.query.text;
+  if (keyword) {
+    const matched = messages.filter((message) => message.text.includes(keyword));
+    res.send(matched);
+  }
+});
+
+app.get("/messages/latest", (req, res) => {
+  res.send(messages.slice(-10));
+});
+
+app.get("/messages/:id", (req, res) => {
+  const msgId = req.params.id;
+  const matched = messages.find((massage) => massage.id + "" === msgId);
+  res.send(matched);
+});
+
+app.post("/messages", (req, res) => {
+  const msg = req.body;
+
+  const valid = !!msg.text && !!msg.from && msg.hasOwnProperty("text") && msg.hasOwnProperty("from");
+  if (!valid) {
+    res.status(400).send("missing information");
+  } else {
+    messages.push(msg);
+    console.log(messages);
+  }
+});
+
+app.delete("/messages/:id", (req, res) => {
+  const msgId = req.params.id;
+  messages = messages.filter((message) => message.id + "" !== msgId);
+  // res.send(messages);
+  console.log(messages);
+});
+
+app.listen(5001 || process.env.PORT);
