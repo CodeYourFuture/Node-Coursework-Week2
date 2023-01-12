07f955212caefbe1cfb871529d11893eb0beb82e
leilafaez <leila.faez@gmail.com>
2023-01-12 19:42:59 +0000

    Complete level 1

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-
+const { request, response } = require("express");
 const app = express();
+
+
+const bodyParser = require("body-parser")
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(cors());
 
+
 const welcomeMessage = {
   id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
+  from: "Neill",
+  text:"hi hi"
 };
-
 //This array is our "data store".
-//We will start with one message in the array.
+//oneWe will start with  message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
@@ -20,4 +25,36 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages",(request,response)=>{
+  response.status(200).json({messages})
+})
+
+app.get("/messages/:id",(request,response)=>{
+  const id= Number(request.params.id);
+  const find= messages.find(message=>message.id===id);
+  response.status(200).json({find})
+})
+
+app.post("/messages",(request,response)=>{
+   if(request.body.from==="" || request.body.text==="" ){
+    response.status(400).json({message : "fill all fields"});
+    return;
+  }
+      const newMessage= {
+    id :messages.length,
+    from : request.body.from, 
+    text : request.body.text
+  };
+  messages.push(newMessage);
+  response.status(201).json({newMessage});
+})
+app.delete("/messages/:id",(request,response)=>{
+  const messageById=messages.find((message)=>message.id===Number(request.params.id));
+  if(messageById<0) return response.status(404).json({msg : "message not found"});
+  messages.splice(messageById,1);
+  response.json({messages})
+})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}.Ready to accept request!`);
+});
