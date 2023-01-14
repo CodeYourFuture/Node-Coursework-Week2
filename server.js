a5281e5ca5a8d9fe5c8896eddf6fdfa80284dc83
Adiba Belayete <adiba.du.bd@gmail.com>
2023-01-14 22:39:29 +0000

    complete level 3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const PORT = 5000;
 const app = express();
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 app.use(express.json());
@@ -12,26 +12,7 @@ app.listen(process.env.PORT || PORT, function () {
   console.log(`Server is listening on port ${PORT}. Ready to accept requests!`);
 });
 
-let welcomeMessage = [
-  {
-    id: 0,
-    from: "Bart",
-    text: "Welcome to CYF chat system!",
-  },
-
-  { id: 1, from: "Eru", text: "Hello! Mamma" },
-  {
-    id: 2,
-    from: "Beru",
-    text: "I am sleepy mamma",
-  },
-  { id: 3, from: "Laru", text: "Hungry!!!!" },
-  { id: 4, from: "Karu", text: "Want to play outside" },
-];
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-let messages = welcomeMessage;
+let messages = require("./messages.json")
 
 //  get the index file
 app.get("/", function (request, response) {
@@ -50,7 +31,7 @@ app.get("/messages/:id", function (request, response) {
     (eachmessage) => eachmessage.id === parseInt(request.params.id)
   );
 
-  // console.log(found);
+  console.log(found);
 
   if (found) {
     response
@@ -76,14 +57,12 @@ app.post("/messages/createNewMessage", function (request, response) {
   };
   if (
     createNewMessage.id === "" ||
-    createNewMessage.from ==="" ||
-    createNewMessage.text ===""
+    createNewMessage.from === "" ||
+    createNewMessage.text === ""
   ) {
-    return response
-      .status(404)
-      .json({
-        message: `Please include a message ID, from who the message is sent & the text`,
-      });
+    return response.status(404).json({
+      message: `Please include a message ID, from who the message is sent & the text`,
+    });
   } else {
     messages.push(createNewMessage);
     response.status(200).json(messages);
@@ -109,4 +88,32 @@ app.delete("/messages/:id", function (request, response) {
       message: `No message with the id of ${request.params.albumId}`,
     });
   }
+});
+
+// chalalnge level 03
+
+// - [ ] Read _only_ messages whose text contains a given substring: `/messages/search?text=express`
+
+app.get("/search", (request, response) => {
+
+  // console.log(request.params.text)
+  // response.send(messages)
+
+  console.log(request.query)
+  const word = request.query.word.toLowerCase();
+  // console.log(word);
+
+  const filteredWord = messages.filter((eachmessage) =>
+    eachmessage.text.toLowerCase().includes(word)
+  );
+
+  response.json(filteredWord);
+  // console.log(filteredWord);
+});
+// - [ ] Read only the most recent 10 messages: `/messages/latest`
+
+app.get("/latest", function (request, response) {
+  const recentMessages = messages.slice(-10);
+  response.send(recentMessages);
+  // console.log(recentMessages);
 });
