e89b456855b1b2c5c1904a93b2c2b6d39c2ed92a
Ekrem <ekremteke.uk@gmail.com>
2023-01-14 01:19:27 +0000

    I added post function and a new message from postman

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9090;
 
-app.use(cors());
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
@@ -18,8 +18,25 @@ const welcomeMessage = {
 const messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
-  response.send("response is sending")
+  response.status(200).send("response is sending")
   response.sendFile(__dirname + "/index.html");
 });
+app.get("/messages", function (request, response) {
+  response.status(200).send({ messages })
+});
 
-app.listen(port);
+app.get("/messages/:id", function (request, response) {
+  const idToFind = Number(request.params.id);
+  const message = messages.find((message) => message.id === idToFind);
+  response.status(200).send({ message })
+});
+
+app.post("/messages", function (request, response) {
+  console.log(request.body)
+  const newMessage = request.body;
+  messages.push(newMessage);
+  response.status(201).send({ newMessage })
+});
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
