d67853444e8eb1784e7267f63a31ffcd3e80f8d3
fatih-celebi <celebifatih.c1@gmail.com>
2023-01-16 23:18:32 +0000

    Complete Level-1

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-
+const bodyParser = require("body-parser");
+let changeID = 0;
 const app = express();
+app.use(express.json());
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const welcomeMessage = {
-  id: 0,
   from: "Bart",
   text: "Welcome to CYF chat system!",
 };
@@ -14,10 +17,48 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", function (request, response) {
+  response.send({ messages });
+});
+
+app.get("/messages/:id", function (request, response) {
+  const messageId = +request.params.id;
+  const oneMessage = messages.find((item) => item.id === messageId);
+  response.send({ oneMessage });
+});
+
+app.post("/messages", function (request, response) {
+  const { from, text } = request.body;
+  const validation =
+    text !== undefined &&
+    text !== "" &&
+    from !== undefined &&
+    from !== "" &&
+    request.body.hasOwnProperty("text") &&
+    request.body.hasOwnProperty("from");
+  if (!validation) {
+    response.status(400).send("Missing Info");
+  } else {
+    changeID++;
+    let createMessage = {
+      id: changeID,
+      from: from,
+      message: text,
+    };
+    messages.push(createMessage);
+    response.status(200).send("Successfully added");
+  }
+});
+app.delete("/messages/:id",(request,response)=>{
+const messageID =+ request.params.id;
+messages = messages.filter(item => item.id !== messageID)
+response.send({ messages });
+});
+
+app.listen(9090);
