e77212ce000149be6cd1f4b0eac7ce389e7388f6
Ekrem <ekremteke.uk@gmail.com>
2023-01-17 19:58:06 +0000

    post and id properties was written

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+
+
 const app = express();
 const port = process.env.PORT || 9090;
 
+app.use(cors());
 app.use(express.json());
+
 
 const welcomeMessage = {
   id: 0,
