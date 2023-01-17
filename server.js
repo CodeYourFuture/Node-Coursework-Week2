32f10844406577f62eec7105e8aa7e9accbdd2ad
ShimenAfshar <afsharshimen@gmail.com>
2023-01-17 20:09:57 +0000

    complete level 1,2,3

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const { response, request } = require("express");
 
 const app = express();
-
+const bodyParser=require("body-parser")
 app.use(cors());
+app.use(express.json())
+app.use(bodyParser.json())
+//data
 
 const welcomeMessage = {
   id: 0,
@@ -11,13 +15,59 @@ const welcomeMessage = {
   text: "Welcome to CYF chat system!",
 };
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
+
+//---data
+
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+app.get("/messages/search",(request,response)=>{
+  const result=request.query.text;
+  let searchResult=messages;
+  if(result){
+    searchResult.filter(item=>item.text.includes(result))
+  }
+  
+  response.send(searchResult)
+})
+app.get("/messages",(request,response)=>{
+  response.send(messages)
+})
 
-app.listen(process.env.PORT);
+app.get("/messages/:id",(request,response)=>{
+  const idParam=Number(request.params.id);
+  const findObject=messages.filter(item=>item.id===idParam)
+  response.send(findObject)
+})
+
+app.post("/messages",(request,response)=>{
+ if(request.body.from==="" || request.body.text===""){
+  response.status(400).send("please fill all the fields")
+  return;
+ }
+  const newMessage={
+    id:messages.length,
+    from:request.body.from,
+    text:request.body.text
+  }
+  messages.push(newMessage);
+  response.send(newMessage);
+})
+app.delete("/messages/:id",(request,response)=>{
+  let idFromUrl=Number(request.params.id);
+  let result=messages.filter(item=>item.id!==idFromUrl)
+  response.send(result)
+})
+
+
+
+
+
+
+
+const port=process.env.PORT|| 3000;
+app.listen(port,function(){
+  console.log(`listen to ${port}`);
+});
