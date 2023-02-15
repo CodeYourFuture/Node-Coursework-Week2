c08d7688285ef3f92c26d7e2c795b6c0e60e708f
Alex Phillip <alexjoelphillip@gmail.com>
2023-02-15 16:48:51 +0000

    Level 1: create new, read all, read one specified by id

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 const welcomeMessage = {
   id: 0,
@@ -16,8 +21,29 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", function (req, res) {
+  res.send(messages)
+})
+
+app.post("/messages", function (req, res) {
+  const newPost = req.body;
+  newPost.id = messages.length;
+  messages.push(newPost);
+})
+
+app.get("/messages/:id", function (req, res) {
+  const id = req.params.id;
+  const message = messages.find((mes) => {
+    return mes.id.toString() === id;
+  });
+  res.send(message);
+})
+
+app.listen(3000, function () {
+  console.log('Server is listening on port 3000');
+});
+
