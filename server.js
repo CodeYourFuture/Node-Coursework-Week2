a2d61e92c202490efcdf925eb45bd9d150fbd180
Lihle Naphakade <31651036+LN2410@users.noreply.github.com>
2023-02-19 23:30:42 +0200

    Add validations and messages read functionality

--- server.js
+++ server.js
@@ -15,19 +15,36 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-let messages = [welcomeMessage];
+let messages = [welcomeMessage, { id: 10, from: "Lihle", text: "testing" }];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
 app.post("/messages", function (request, response) {
+  if (request.body.from.trim() === "" || request.body.text.trim() === "") {
+    return response.status(404).send("Name and Message cannot be empty");
+  }
   messages.push({ id: messages.length, ...request.body });
   response.status(200).redirect("/messages");
 });
 
 app.get("/messages", function (request, response) {
   response.json(messages);
+});
+
+app.get("/messages/search", function (request, response) {
+  const filteredMessages = messages.filter((el) => {
+    return el.text.toLowerCase().includes(request.query.text.toLowerCase());
+  });
+  response.send(filteredMessages);
+});
+
+app.get("/messages/latest", function (request, response) {
+  const topTen = messages.slice(
+    messages.length > 10 ? messages.length - 10 : 0
+  );
+  response.send(topTen);
 });
 
 app.get("/messages/:id", function (request, response) {
@@ -37,7 +54,6 @@ app.get("/messages/:id", function (request, response) {
 
 app.get("/messages/delete/:id", function (request, response) {
   const id = request.params.id;
-  console.log(typeof id);
   messages = messages.filter((_, index) => {
     return index != id;
   });
