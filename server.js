4ebff928786347d72de05c4fb24d44ac865ced26
Andriana <andrianasaffo@gmail.com>
2023-03-28 14:15:17 +0100

    Levels 1 and 2 complete

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const { request, response } = require("express");
 
 const app = express();
+const PORT = process.env.PORT || 3003;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
 
@@ -20,4 +25,56 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.post("/messages", function (request, response) {
+  const newWelcomeMessage = {
+    id: Math.floor(Math.random() * messages.length) + messages.length,
+    from: request.body.from,
+    text: request.body.text,
+  };
+
+  if (!newWelcomeMessage.from || !newWelcomeMessage.text) {
+    return response
+      .status(400)
+      .json({ message: "Please include name and message" });
+  }
+
+  messages.push(newWelcomeMessage);
+  response.send(messages);
+});
+
+app.get("/messages", function (request, response) {
+  response.send(messages);
+});
+
+app.get("/messages/:id", function (request, response) {
+  const foundMessage = messages.find(
+    (eachMessage) => eachMessage.id === parseInt(request.params.id)
+  );
+
+  foundMessage
+    ? response.json(foundMessage)
+    : response.status(400).json({ message: "Message not found" });
+});
+
+app.delete("/messages/:id", function (request, response) {
+  const foundMessage = messages.find(
+    (eachMessage) => eachMessage.id === parseInt(request.params.id)
+  );
+
+  if (foundMessage) {
+    response.json({
+      message: `Message ${request.params.id} deleted`,
+      messages: messages.filter(
+        (eachMessage) => eachMessage.id !== parseInt(request.params.id)
+      ),
+    });
+  } else {
+    response
+      .status(400)
+      .json({ message: `Message ${request.params.id} not found` });
+  }
+});
+
+app.listen(PORT, function () {
+  console.log(`Listening on ${PORT}`);
+});
