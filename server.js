d31e534e1cd56e4839581d01614c8bfb67f86a0b
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-03-30 21:32:40 +0100

    Adding api endpoints, nodemon

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const port = 3030;
 
 const app = express();
+
+app.use(express.json());
 
 app.use(cors());
 
@@ -16,8 +19,41 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+//Create a new message
+app.post("/messages", function (req, res) {
+  const newUser = req.body;
+  messages.push(newUser);
+  res.status(201).send(newUser);
+});
+
+//Read all messages
+app.get("/messages", function (req, res) {
+  res.status(200).send(messages);
+});
+
+//Read one message specified by an ID
+app.get("/messages/:id", function (req, res) {
+  const userId = Number(req.params.id);
+  const foundedMessage = messages.find((message) => message.id === userId);
+  res.status(200).send(foundedMessage);
+});
+
+//Delete a message, by ID
+app.delete("/messages/:id", function (req, res) {
+  const userId = Number(req.params.id);
+  messages.forEach((message) => {
+    if (message.id === userId) {
+      const index = messages.indexOf(message);
+      messages.splice(index, 1);
+    }
+  });
+  res.status(204).send("The message was deleted");
+});
+
+app.listen(port, () => {
+  console.log("Listening to the port 3030...");
+});
