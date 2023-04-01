e9d6074b3b06d2c87ecfdf349535bdb51b4f40d1
laxmikckarki <108900563+laxmikckarki@users.noreply.github.com>
2023-04-01 22:36:53 +0100

    done level 3

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(express.json()); // before our routes definition
 
+app.use(express.json()); // before our routes definition
 app.use(cors());
 
 //This array is our "data store".
@@ -26,10 +26,11 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
+//[2] read all messages
 app.get("/messages", (req, res) => {
   res.status(200).json(messages);
 });
-
+//[3] read one message specified by id
 app.get("/messages/:messageId", (req, res) => {
   const messageId = parseInt(req.params.messageId);
   const foundMessage = messages.find((messageItem) => {
@@ -37,7 +38,7 @@ app.get("/messages/:messageId", (req, res) => {
   });
   res.json(foundMessage);
 });
-//post and validate(level 1, 2)
+//[1] create a new message,post and validate(level 1, 2)
 app.post("/messages", function (req, res) {
   const newMessage = req.body;
   if (!newMessage.from || !newMessage.text) {
@@ -49,13 +50,32 @@ app.post("/messages", function (req, res) {
   }
 });
 
-// delete
+// [4]delete a message by id
 app.delete("/messages/:messageId", function (req, res) {
   const messageId = parseInt(req.params.messageId);
   messages = messages.filter((messageItem) => {
     return messageItem.id !== messageId;
   });
   res.json({ message: `message${messageId} deleted`, messages });
+});
+//level 3 [1]Read only messages whose text contains a given substring:
+app.get("/messages/search", (req, res) => {
+  const { term } = req.query;
+  console.log(term);
+
+  const filterMessages = messages.filter((message) => {
+    message.text.toLowerCase().includes(term.toLowerCase());
+  });
+  console.log(filterMessages);
+  res.send(filterMessages);
+});
+
+//level 3 [2]read only the most recent 10 messages
+app.get("/messages/latest", (req, res) => {
+  const filterMessages = messages.filter(
+    (message, index) => messages.length - 10 <= index
+  );
+  res.send(filterMessages);
 });
 
 //app.listen(process.env.PORT);
