55ea8613c599f8d598d39fc3f63a83b9b75f5724
laxmikckarki <108900563+laxmikckarki@users.noreply.github.com>
2023-04-01 13:44:04 +0100

    done level 1

--- server.js
+++ server.js
@@ -2,22 +2,58 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+app.use(express.json()); // before our routes definition
 
 app.use(cors());
-
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
 
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [
+  {
+    id: 0,
+    from: "Bart",
+    text: "Welcome to CYF chat system!",
+  },
+  {
+    id: 1,
+    from: "Joe",
+    text: "New Message!",
+  },
+];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages", (req, res) => {
+  res.status(200).json(messages);
+});
+
+app.get("/messages/:messageId", (req, res) => {
+  const messageId = parseInt(req.params.messageId);
+  const foundMessage = messages.find((messageItem) => {
+    return messageItem.id === messageId;
+  });
+  res.json(foundMessage);
+});
+
+app.post("/messages", function (req, res) {
+  const newMessage = req.body;
+  newMessage.id = messages.length;
+  messages.push(newMessage);
+  res.json("messageCreate");
+});
+// notice .delete
+app.delete("/messages/:messageId", function (req, res) {
+  const messageId = parseInt(req.params.messageId);
+  messages = messages.filter((messageItem) => {
+    return messageItem.id !== messageId;
+  });
+  res.json("message deleted");
+});
+
+//app.listen(process.env.PORT);
+app.listen(3001, () => {
+  console.log("The server is running on port 3001");
+});
