dbce5cd800342c705db2b70cc6c7e17ce2ab1dde
ZahraaTayyar <zahraatayyar@gmail.com>
2023-04-03 16:44:18 +0100

    level 5 completed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { response } = require("express");
+const path = require("path");
 
 const app = express();
 app.use(cors());
@@ -9,7 +10,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const welcomeMessage = {
   id: 0,
-  from: "Bart",
+  from: "Zahraa",
   text: "Welcome to CYF chat system!",
 };
 
@@ -23,6 +24,7 @@ app.get("/", (request, response) => {
   response.send("You are now live");
 });
 
+//CREATE message and POST it
 app.post("/messages", (req, res) => {
   let newMessage = req.body;
 
@@ -42,10 +44,12 @@ app.post("/messages", (req, res) => {
   }
 });
 
+//GET all messages
 app.get("/messages", (req, res) => {
   res.status(200).json(messages);
 });
 
+//GET message specified by search query
 app.get("/messages/search", (req, res) => {
   const searchQuery = req.query.text;
 
@@ -56,6 +60,7 @@ app.get("/messages/search", (req, res) => {
   res.status(200).json(matchedMessages);
 });
 
+//GET latest 10 messages
 app.get("/messages/latest", (req, res) => {
   if (messages.length === 0) {
     res.status(200).send(error, "There are no messages");
@@ -70,13 +75,43 @@ app.get("/messages/latest", (req, res) => {
   }
 });
 
+//GET message specified by ID
 app.get("/messages/:id", (req, res) => {
-  // you need to check that the value of "idToFind" is actually a number, so we have to use the "Number()" method.
+  // you need to check that the value of "idToFind" is actually a number, so we have to use the "Number()" method:
   const idToFind = Number(req.params.id);
   const message = messages.find((msg) => msg.id === idToFind);
   res.status(200).json(message);
 });
 
+//UPDATE message by specified ID
+app.put("/messages/:id", (req, res) => {
+  const messageId = Number(req.params.id);
+  const messageToUpdate = messages.find((msg) => msg.id === messageId);
+  const indexOfMessageToUpdate = messages.indexOf(messageToUpdate);
+
+  let updatedMessage = {
+    id: messageId,
+    from: req.body.from,
+    text: req.body.text,
+    time: messageToUpdate.time,
+  };
+
+  if (indexOfMessageToUpdate === -1) {
+    res
+      .status(404)
+      .json({ success: false, error: "No message with that ID was found" });
+  } else if (indexOfMessageToUpdate === 0) {
+    res
+      .status(404)
+      .json({ success: false, error: "You cannot edit this message" });
+  } else {
+    messages.splice(indexOfMessageToUpdate, 1, updatedMessage);
+  }
+
+  res.status(200).json({ success: true, updatedMessage });
+});
+
+//DELETE message specified by ID
 app.delete("/messages/:id", (req, res) => {
   const idToDelete = Number(req.params.id);
   const indexOfMessageToDelete = messages.findIndex(
