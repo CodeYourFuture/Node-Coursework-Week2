7ab75092c04bc3dbf3a53d95a6d168107e5c8e6c
Mari Azh <Marzi.aghchay@gmail.com>
2023-04-03 09:46:19 +0100

    step 1

---
+++ script.js
@@ -0,0 +1,28 @@
+const nameId = document.getElementById("name-id");
+const messageId = document.getElementById("message-id");
+let formFrom = "";
+let formText = "";
+
+console.log(messageId.value);
+
+const sendMessage = (e) => {
+  e.preventDefault();
+  console.log("you've clicked me");
+  console.log(formFrom);
+  console.log(formText);
+
+  fetch("http://localhost:4000/messages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ from: formFrom, text: formText }),
+  });
+};
+
+const sendButton = document.getElementById("send-id");
+
+sendButton.addEventListener("click", sendMessage);
+
+nameId.addEventListener("keyup", (e) => (formFrom = e.target.value));
+messageId.addEventListener("keyup", (e) => (formText = e.target.value));
