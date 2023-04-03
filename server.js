4547fd7cb965046c9f3ac38fa64ad7abdd45267c
Mo Mahaboobian <momahaboobian@MoReels-iMac.local>
2023-04-03 21:21:43 +0100

    Level 1

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const cors = require("cors");
 
 const app = express();
 
+// const port = 3000;
+
 app.use(cors());
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
@@ -15,9 +18,56 @@ const welcomeMessage = {
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
+let nextMessageId = 1;
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Create a new message
+app.post("/messages", (req, res) => {
+  const { id, from, text } = req.body;
+
+  const newMessage = {
+    id: nextMessageId++,
+    from,
+    text,
+  };
+  messages.push(newMessage);
+
+  res.json(messages);
+});
+
+//  Read all messages
+app.get("/messages", (req, res) => {
+  res.json(messages);
+});
+
+// Read one message specified by an ID
+app.get("/messages/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const message = messages.find((msg) => msg.id === id);
+  if (message) {
+    res.json(message);
+  } else {
+    res.status(404).send("Message not found");
+  }
+});
+
+// Delete a message, by ID
+app.delete("/messages/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = messages.findIndex((msg) => msg.id === id);
+  if (index >= 0) {
+    messages.splice(index, 1);
+    res.status(204).send("Message has been deleted");
+  } else {
+    res.status(404).send("Message not found");
+  }
 });
 
 app.listen(process.env.PORT);
+
+// app.listen(port, () => {
+//   console.log("Server listening on port 3000...");
+// });
