a902f31eff7ead8cf9ece1574e2cc05bc5fc793a
Meysam-Arshadi <meysamarshadi@gmail.com>
2023-04-04 20:17:33 +0100

    Level one\first 2 parts done

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const cors = require("cors");
 
 const app = express();
 
+const port = 3000;
+
 app.use(cors());
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
@@ -15,9 +18,28 @@ const welcomeMessage = {
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
+let nextMessageID = 1;
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.post("/message", (req, res) => {
+  const newMessage = req.body;
+  messages.id = nextMessageID;
+  messages.push(newMessage);
+  nextMessageID++;
+
+  res.send(messages);
+});
+
+app.get("/message", (req, res) => {
+  res.json(messages);
+});
+
+// app.listen(process.env.PORT)
+app.listen(port, () => {
+  console.log("Server is listening to the port 3000...");
+});
+
+//
