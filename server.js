65e1f10568193d26a6d07535d6f0c608519498eb
tony-dev-wm <ta.devopsuk@gmail.com>
2023-04-05 15:13:35 +0100

    Chat server with crud methodologies implemented

--- server.js
+++ server.js
@@ -25,25 +25,86 @@ app.get("/", function (request, response) {
 
 
 // Create a new message
-app.post('/messages', function(req,res) =>{
+app.post('/messages', function(req,res) {
 const newMessage = {
   id:messages.length,
   from:req.body.from,
   text: req.body.text,
-  timeSent:newD
-}  
+  timeSent:new Date().toLocaleString(),
+};
+if(!newMessage.from || !newMessage.text) {
+  return response
+  .status(400)
+  .json({message: "Name and Message needed to proceed"});
+}
+messages.push(newMessage);
+res.send(messages);
 })
 
 
 // - [ ] Read all messages
+app.get('/messages', function(req,res) {
+  res.send(messages);
+})
 
+// - [ ] Messages based on search term 
+app.get('/messages/search', function(req,res) {
+  const searchMessage = req.query.term;
 
+  function searchTerm (array) {
+    return array.filter((word)=>
+    word.text.toLowerCase().includes(searchMessage.toLowerCase()));
+  }
+  res.send(searchTerm(messages));
+});
 
+//-[ ] 10 recent messages
+app.get('/messages/last', function (req,res){
+  const lastMessages = messages.slice(-10);
+  res.send(lastMessages);
+});
 // - [ ] Read one message specified by an ID
+app.get('/messages/:id', function(req,res){
+  const messageWithId= messages.find((x)=> x.id === Number(req.params.id));
 
+  messageWithId ?
+  res.json(messageWithId):
+  res.status(400).json({message: 'No Message Found with that Id'});
+});
 
 
 // - [ ] Delete a message, by ID
+app.delete('/messages/:id', function (req,res){
+  const selectMessage= messages.find((x) => x.id === Number(req.params.id));
+
+  if(selectMessage){
+    res.json({
+      message: `Message with ${req.params.id} is no more`,
+      messages:messages.filter((x)=> x.id !== Number(req.params.id)),
+    }); 
+  } else {
+    res.status(400).json({message: `Message ${req.params.id} could not be located`});
+  }
+});
+
+//- [ ] Update Messages
+app.put('/messages/:id', function (req, res) {
+  const selectMessage = messages.find((x) => x.id ===Number(req.params.id));
+  if (selectMessage) {
+    const updateMessage = req.body;
+
+    messages.forEach((x) => {
+      if (x.id ===Number(req.params.id)) {
+        x.from = updateMessage.from ? updateMessage.from : x.from;
+          x.text = updateMessage.text ? updateMessage.text : x.text;
+
+          res.json({msg:'Message has been updated', x});
+      } else {
+        res.status(400).json({message:'No Message Found'})
+      }
+    });
+  }
+});
 
 app.listen(PORT, function () {
   console.log(`Listening on ${PORT}`);
