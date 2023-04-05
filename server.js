b16aa5ef94d2a84748368c13aa649bf1cd9a9af2
lexiisying <nyxxing0824@gmail.com>
2023-04-05 17:56:48 +0100

    level 1

--- server.js
+++ server.js
@@ -19,19 +19,59 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/messages", function(request,response) {
-  response.json(messages);
+// read all messages
+app.get("/messages", function(req,res) {
+  res.json(messages);
 })
 
-app.post("/messages", function (request, response) {
-  console.log(request.body);
-  response.json(request.body);
-  messages.push(request.body);
-  console.log(messages);
+// read a message by id
+app.get("/messages/:id", function(req, res) {
+  const messageId = req.params.id;
+  const findId = messages.find((message) => message.id === messageId);
+    if (findId) {
+    res.json(findId);
+    console.log(findId);
+  } else {
+    res.status(404).send("Message not found");
+  }
 });
+
+
+// create a new message
+app.post("/messages", function (req, res) {
+  const {from, text} = req.body
+  const newMessage = {
+     id: messages.length,
+     from,
+     text,
+   };
+
+  console.log(newMessage);
+  messages.push(newMessage);
+  res.status(201).json({ success: true, data: messages });
+  // response.json(request.body);
+  // messages.push(request.body);
+  //console.log(messages);
+});
+
+// delete message by id
+app.delete("/messages/:id", function (req, res) {
+  const messageId = req.params.id;
+  const findIndex = messages.findIndex((message) => message.id === messageId);
+
+  if (findIndex === -1) {
+    res.status(404).send("Message not found");
+  } else {
+    messages.splice(findIndex, 1);
+    res.status(204).send();
+  }
+});
+
+
+
 
 app.listen(process.env.PORT);
