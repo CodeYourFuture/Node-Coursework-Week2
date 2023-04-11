debe3bb1a31e9d1ee044a7c825263af858e652b8
Meysam-Arshadi <meysamarshadi@gmail.com>
2023-04-11 15:47:45 +0100

    Level 1 to 5 Done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const res = require("express/lib/response");
 
 const app = express();
 
@@ -7,39 +8,70 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
-
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
-let nextMessageID = 1;
+const messages = [];
+let nextMessageID = 0;
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.post("/message", (req, res) => {
-  const newMessage = req.body;
-  messages.id = nextMessageID;
-  messages.push(newMessage);
-  nextMessageID++;
-
-  res.send(messages);
+app.post("/messages", (req, res) => {
+  if (req.body.from !== "" && req.body.text !== "") {
+    nextMessageID++;
+    const message = {};
+    message.id = nextMessageID;
+    message.from = req.body.from;
+    message.text = req.body.text;
+    message.timeSent = new Date().toString();
+    messages.push(message);
+    res.send(messages);
+  } else {
+    res.sendStatus(400);
+  }
 });
 
-app.get("/message", (req, res) => {
+app.get("/messages", (req, res) => {
   res.json(messages);
 });
 
-// app.listen(process.env.PORT)
+app.get("/messages/search", (req, res) => {
+  const searchText = messages.filter((message) =>
+    message.text.includes(req.query.text)
+  );
+  res.json(searchText);
+});
+
+app.get("/message/:id", (req, res) => {
+  const id = req.params.id;
+  const messageByID = messages.filter((message) => message.id == id);
+  res.json(messageByID);
+});
+
+app.put("/messages/update/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const messageIndex = messages.findIndex((message) => message.id === id);
+  messages[messageIndex].text = req.body.text;
+  res.json(messages[messageIndex]);
+});
+
+app.delete("/message/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const deleteMessageByID = messages.filter((message) => message.id !== id);
+  res.json(deleteMessageByID);
+});
+
+app.get("/messages/latest/:number", (req, res) => {
+  const number = Number(req.params.number);
+  if (messages.length >= number) {
+    const latestMessages = messages.slice(-number);
+    res.json(latestMessages);
+  } else {
+    res.json(messages);
+  }
+});
+
 app.listen(port, () => {
   console.log("Server is listening to the port 3000...");
 });
-
-//
