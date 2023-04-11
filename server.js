423bfa01f4b97555ed6b738ff6c022bc5e571d92
Olena Reukova <kronverk01@gmail.com>
2023-04-11 19:02:28 +0100

    Level 2 - simple validation,Level 3 - more "read" functionality

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -24,12 +25,19 @@ app.get("/hello", function (request, response) {
   response.send("Hello!");
 });
 
-app.use(express.urlencoded({ extended: true })); // for parsing application/json
+//app.use(express.urlencoded({ extended: true })); // for parsing application/json
+app.use(bodyParser.json());
 
 let idCounter = 1;
 
 // create a new message
 app.post("/messages", function (request, response) {
+  if (!request.body.from || !request.body.text) {
+    response.status(400).send(); //- [ ] _reject_ requests to create messages if the message objects have an empty or missing `text` or `from` property.
+    //- [ ] In this case your server should return a status code of `400`
+    return;
+  }
+  console.log("post", request.body);
   console.log(request.body);
 
   const newMessage = {
@@ -42,9 +50,17 @@ app.post("/messages", function (request, response) {
   response.redirect("/");
 });
 
-// read all messages
+//read all messages
 app.get("/messages", function (request, response) {
   response.json(messages);
+});
+
+app.get("/messages/search", function (request, response) {
+  let filteredMessages = messages.filter(
+    (message) => message.text.includes(request.query.text) //- [ ] Read _only_ messages whose text contains a given substring: `/messages/search?text=express`
+  );
+
+  response.json(filteredMessages);
 });
 
 // read a message by id
