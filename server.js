0c02e4d906ecf0c137f6a3e71b28953f75c48ed9
Gayle Ngozi <gaylengozi@gmail.com>
2023-04-15 14:08:19 +0100

    level one cpmpleted

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
@@ -14,10 +15,60 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+
+// app.get("/", function (request, response) {
+//   response.send("Yay Node!");
+// });
+
+let messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
+  // response.send("hello gayle");
+});
+// app.get("/", function (request, response) {
+//   response.send(`Hello, welcome to node ${messages}`);
+// });
+
+app.get("/messages", function (request, response) {
+  response.json(messages);
 });
 
-app.listen(process.env.PORT);
+app.post("/messages", function (request, response) {
+  const NewMessage = {
+    id: messages.length,
+    from: request.body.from,
+    text: request.body.text,
+  };
+  messages.push(NewMessage);
+  response.json(messages);
+});
+
+app.get("/messages/:id", function (request, response) {
+  const messageId = request.params.id;
+  const oneMessage = messages.find(
+    (eachMessage) => eachMessage.id === parseInt(messageId)
+  );
+  oneMessage
+    ? response.json(oneMessage)
+    : response.json({ message: "message not found" });
+});
+
+app.delete("/messages/:id", function (request, response) {
+  const messageId = request.params.id;
+  const oneMessage = messages.find(
+    (eachMessage) => eachMessage.id === parseInt(messageId)
+  );
+  if (oneMessage) {
+    messages = messages.filter(
+      (eachMessage) => eachMessage.id !== parseInt(messageId)
+    );
+    response.json({ message: "message deleted" });
+  } else {
+    response.json({ message: "message not found" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}. Ready to accept requests!`);
+});
