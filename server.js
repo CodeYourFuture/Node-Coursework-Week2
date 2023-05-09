8a78297ee243c469bd96643d0798c69383689529
Zobeir-Rigi <rigi.zobir@yahoo.com>
2023-05-09 19:22:30 +0100

    done

--- server.js
+++ server.js
@@ -24,12 +24,37 @@ app.get("/", function (request, response) {
 /******************************* creating a message */
 app.post("/messages", (req, res) => {
   console.log(req.body);
-  res.json("i've got you'r message!")
+  const newmessage = req.body
+  messages.push(newmessage)
+  res.json({messages})
 });
+
 /********************************get all messages */
 app.get("/messages", (req, res) => {
   res.json({messages});
 });
+
+app.get("/messages/:id", (req,res)=>{
+    const myId = req.params.id;
+
+    const specificMessage= messages.find((el)=> el.id == myId);
+    if (specificMessage){
+    res.json({specificMessage})
+    }else {
+      res.json({error:"error"},404)
+    }
+})
+
+app.delete("/messages/:id" , (req,res) =>{
+  const deleteMessage = req.params.id;
+  const indexToDelete = messages.findIndex((el)=> el.id == deleteMessage);
+  if(indexToDelete !== -1){
+  messages.splice(indexToDelete,1);
+  res.json({messages});
+  }else{
+          res.json({ error: "error" }, 404);
+  }
+})
 
 
 // app.listen(process.env.PORT);
