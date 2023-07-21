8ba0ce19ba200e4fdc3b8777e05d45a081de7d7e
Kristina <kristina.dudnyk20@gmail.com>
2023-07-21 21:49:20 +0100

    implementation of get and post request

--- server.js
+++ server.js
@@ -14,10 +14,35 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+// const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  console.log("Server is up");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/messages", (req, res) => {
+  console.log();
+  res.status(200).send(welcomeMessage);
+});
+
+app.post("/messages", (req, res) => {
+  const { from: from, text: text } = req.body;
+
+  if (form && text) {
+    welcomeMessage.push({
+      id: welcomeMessage.length + 1,
+      from: from,
+      text: text,
+    });
+    res.status(201).redirect("/");
+  } else if (!form) {
+    res.status(400).send("You missing your name");
+  } else if (!text) {
+    res.status(404).send("You missing your message");
+  } else if (!form && !text) {
+    res.status(400).send("Your missing everything");
+  }
 });
 
 app.listen(process.env.PORT);
